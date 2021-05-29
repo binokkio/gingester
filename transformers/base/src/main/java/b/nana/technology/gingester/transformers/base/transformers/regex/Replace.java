6eb38fe0b5c3d7dccfc641502cde055b0ac0db2d
Binokkio <binokkio@b.nana.technology>
2021-05-29 19:57:58 +0200

    Implemented Regex.Replace

--- transformers/base/src/main/java/b/nana/technology/gingester/transformers/base/transformers/regex/Replace.java
+++ transformers/base/src/main/java/b/nana/technology/gingester/transformers/base/transformers/regex/Replace.java
@@ -2,10 +2,36 @@ package b.nana.technology.gingester.transformers.base.transformers.regex;
 
 import b.nana.technology.gingester.core.Context;
 import b.nana.technology.gingester.core.Transformer;
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+import java.util.regex.Pattern;
 
 public class Replace extends Transformer<String, String> {
+
+    private final Pattern pattern;
+    private final String replacement;
+
+    public Replace(Parameters parameters) {
+        pattern = Pattern.compile(parameters.pattern);
+        replacement = parameters.replacement;
+    }
+
     @Override
     protected void transform(Context context, String input) throws Exception {
-        throw new UnsupportedOperationException("Not yet implemented");
+        emit(context, pattern.matcher(input).replaceAll(replacement));
+    }
+
+    public static class Parameters {
+
+        public String pattern;
+        public String replacement = "_";
+
+        @JsonCreator
+        public Parameters() {}
+
+        @JsonCreator
+        public Parameters(String pattern) {
+            this.pattern = pattern;
+        }
     }
 }
