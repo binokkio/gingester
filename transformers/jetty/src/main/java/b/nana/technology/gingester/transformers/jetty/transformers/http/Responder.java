16fa29dcfd83be445a865e33d822aae3b2b587af
Binokkio <binokkio@b.nana.technology>
2021-06-05 15:58:02 +0200

    Used new exception handling in Jetty Http.Responder

--- transformers/jetty/src/main/java/b/nana/technology/gingester/transformers/jetty/transformers/http/Responder.java
+++ transformers/jetty/src/main/java/b/nana/technology/gingester/transformers/jetty/transformers/http/Responder.java
@@ -4,20 +4,47 @@ import b.nana.technology.gingester.core.Context;
 import b.nana.technology.gingester.core.Transformer;
 import jakarta.servlet.http.HttpServletResponse;
 
-import java.util.NoSuchElementException;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Responder extends Transformer<Object, Void> {
 
-    @Override
-    protected void transform(Context context, Object input) {}
+    private final Map<Context, List<Throwable>> contextExceptions = Collections.synchronizedMap(new HashMap<>());
 
     @Override
-    protected void finish(Context context) throws Exception {
-        super.finish(context);
+    protected void prepare(Context context) {
+        // TODO the list should be synchronized as well if exceptions from async linked transformations bubble up to here..
+        Object collision = contextExceptions.put(context, new ArrayList<>());
+        if (collision != null) throw new IllegalStateException("Collision");
+    }
 
-        if (context.fetch("response").isEmpty() && Math.random() < .2) return;
+    @Override
+    protected void transform(Context context, Object input) {
+        context.fetch("exception").ifPresent(exception -> {
+            for (Context parent : context) {
+                List<Throwable> collector = contextExceptions.get(parent);
+                if (collector != null) {
+                    collector.add((Throwable) exception);
+                    break;
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void finish(Context context) throws IOException {
 
         HttpServletResponse response = (HttpServletResponse) context.fetch("response").orElseThrow(() -> new NoSuchElementException("Nothing to respond to"));
-        response.setStatus(400);
+
+        List<Throwable> exceptions = contextExceptions.get(context);
+
+        if (!exceptions.isEmpty()) {
+            response.setStatus(409);
+            response.getOutputStream().write(exceptions.stream()
+                    .map(t -> t.getClass().getSimpleName() + ": " + t.getMessage())
+                    .collect(Collectors.joining("\n", "", "\n")).getBytes(StandardCharsets.UTF_8));
+        }
     }
 }
