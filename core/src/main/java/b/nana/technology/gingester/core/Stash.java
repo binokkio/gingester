6092c5716d9488973d624cf30a3b108d4650a844
Binokkio <binokkio@b.nana.technology>
2021-06-15 03:38:38 +0200

    Stash can now store a value from its parameters

--- core/src/main/java/b/nana/technology/gingester/core/Stash.java
+++ core/src/main/java/b/nana/technology/gingester/core/Stash.java
@@ -7,6 +7,7 @@ import java.util.Map;
 public final class Stash<T> extends Passthrough<T> {
 
     private final String stash;
+    private final Object value;
 
     public Stash() {
         this(new Parameters());
@@ -15,13 +16,15 @@ public final class Stash<T> extends Passthrough<T> {
     public Stash(Parameters parameters) {
         super(parameters);
         stash = parameters.stash;
+        value = parameters.value;
         if (stash.contains(".")) throw new IllegalArgumentException("'.' not allowed in stash name");  // TODO
     }
 
     @Override
     protected void transform(Context context, T input) throws Exception {
         emit(
-                context.extend(this).description("stash " + stash).stash(Map.of(stash, input)),
+                context.extend(this).description("stash " + stash)
+                        .stash(Map.of(stash, value != null ? value : input)),
                 input
         );
     }
@@ -29,6 +32,7 @@ public final class Stash<T> extends Passthrough<T> {
     public static class Parameters {
 
         public String stash = "stash";
+        public Object value;
 
         @JsonCreator
         public Parameters() {}
