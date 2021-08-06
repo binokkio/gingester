25dcb8f99d2d6ff4ba7123104c2f00059da88665
Binokkio <binokkio@b.nana.technology>
2021-08-06 07:47:04 +0200

    Added RabbitMQ.Write

---
+++ transformers/rabbitmq/src/main/java/b/nana/technology/gingester/transformers/rabbitmq/Resolver.java
@@ -0,0 +1,13 @@
+package b.nana.technology.gingester.transformers.rabbitmq;
+
+import java.util.Map;
+import java.util.Set;
+
+public class Resolver extends b.nana.technology.gingester.core.Resolver {
+    public Resolver() {
+        super(
+                Set.of("b.nana.technology.gingester.transformers.rabbitmq.transformers"),
+                Map.of("rabbitmq", "RabbitMQ")
+        );
+    }
+}
---
+++ transformers/rabbitmq/src/main/java/b/nana/technology/gingester/transformers/rabbitmq/transformers/rabbitmq/Write.java
@@ -0,0 +1,43 @@
+package b.nana.technology.gingester.transformers.rabbitmq.transformers.rabbitmq;
+
+import b.nana.technology.gingester.core.Context;
+import b.nana.technology.gingester.transformers.rabbitmq.common.RabbitmqTransformer;
+import com.rabbitmq.client.GetResponse;
+
+public class Write extends RabbitmqTransformer<byte[], byte[]> {
+
+    private final String exchange;
+    private final String routingKey;
+    private final boolean mandatory;
+    private final boolean immediate;
+
+    public Write(Parameters parameters) {
+        super(parameters);
+        exchange = parameters.exchange;
+        routingKey = parameters.routingKey;
+        mandatory = parameters.mandatory;
+        immediate = parameters.immediate;
+    }
+
+    @Override
+    protected void transform(Context context, byte[] input) throws Exception {
+
+        getChannel().basicPublish(
+                exchange,
+                routingKey,
+                mandatory,
+                immediate,
+                null,
+                input
+        );
+
+        emit(context, input);
+    }
+
+    public static class Parameters extends RabbitmqTransformer.Parameters {
+        public String exchange = "";
+        public String routingKey = "";
+        public boolean mandatory;
+        public boolean immediate;
+    }
+}
