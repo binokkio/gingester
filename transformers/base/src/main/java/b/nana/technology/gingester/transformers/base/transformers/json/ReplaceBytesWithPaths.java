cdba7a077372bed599c49395b53c574bc6c35487
Binokkio <binokkio@b.nana.technology>
2021-08-17 19:17:22 +0200

    Improved Json.ReplaceBytesWithPaths, added proper tests

--- transformers/base/src/main/java/b/nana/technology/gingester/transformers/base/transformers/json/ReplaceBytesWithPaths.java
+++ transformers/base/src/main/java/b/nana/technology/gingester/transformers/base/transformers/json/ReplaceBytesWithPaths.java
@@ -10,24 +10,29 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.*;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Set;
+import java.util.regex.Pattern;
 
 public class ReplaceBytesWithPaths extends Transformer<JsonNode, JsonNode> {
 
     private final Path directory;
+    private final Pattern filenameReplacePattern;
     private final String extension;
-    private final Path relativeTo;
     private final OpenOption[] openOptions;
+    private final Path pathRelativeTo;
 
     public ReplaceBytesWithPaths(Parameters parameters) {
         super(parameters);
 
         directory = Paths.get(parameters.directory);
+        filenameReplacePattern = Pattern.compile(parameters.filenameReplacePattern);
         extension = parameters.extension;
-        relativeTo = Paths.get(parameters.relativeTo);
         openOptions = parameters.openOptions;
+        pathRelativeTo = Paths.get(parameters.pathRelativeTo != null ? parameters.pathRelativeTo : parameters.directory);
 
-        if (!directory.startsWith(relativeTo)) {
+        if (!directory.startsWith(pathRelativeTo)) {
             throw new IllegalStateException("!directory.startsWith(relativeTo)");
         }
 
@@ -40,11 +45,11 @@ public class ReplaceBytesWithPaths extends Transformer<JsonNode, JsonNode> {
 
     @Override
     protected void transform(Context context, JsonNode input) throws Exception {
-        transform(input, "");
+        transform(input, "", new HashSet<>());
         emit(context, input);
     }
 
-    private void transform(JsonNode jsonNode, String jsonPointer) throws IOException {
+    private void transform(JsonNode jsonNode, String jsonPointer, Set<Path> usedPaths) throws IOException {
         if (jsonNode.isObject()) {
             Iterator<String> keys = jsonNode.fieldNames();
             while (keys.hasNext()) {
@@ -52,11 +57,11 @@ public class ReplaceBytesWithPaths extends Transformer<JsonNode, JsonNode> {
                 String childJsonPointer = jsonPointer + "/" + key;
                 JsonNode child = jsonNode.get(key);
                 if (child.isBinary()) {
-                    Path path = directory.resolve(childJsonPointer.substring(1).replaceAll("[^A-Za-z0-9]", "-") + extension);
+                    Path path = findUnusedPath(childJsonPointer, usedPaths);
                     Files.write(path, ((BinaryNode) child).binaryValue(), openOptions);
-                    ((ObjectNode) jsonNode).put(key, relativeTo.relativize(path).toString());
+                    ((ObjectNode) jsonNode).put(key, pathRelativeTo.relativize(path).toString());
                 } else if (child.isContainerNode()) {
-                    transform(child, childJsonPointer);
+                    transform(child, childJsonPointer, usedPaths);
                 }
             }
         } else if (jsonNode.isArray()) {
@@ -64,20 +69,31 @@ public class ReplaceBytesWithPaths extends Transformer<JsonNode, JsonNode> {
                 String childJsonPointer = jsonPointer + "/" + i;
                 JsonNode child = jsonNode.get(i);
                 if (child.isBinary()) {
-                    Path path = directory.resolve(childJsonPointer.substring(1).replaceAll("[^A-Za-z0-9]", "-") + extension);
+                    Path path = findUnusedPath(childJsonPointer, usedPaths);
                     Files.write(path, ((BinaryNode) child).binaryValue(), openOptions);
-                    ((ArrayNode) jsonNode).set(i, ((ArrayNode) jsonNode).textNode(relativeTo.relativize(path).toString()));
+                    ((ArrayNode) jsonNode).set(i, ((ArrayNode) jsonNode).textNode(pathRelativeTo.relativize(path).toString()));
                 } else if (child.isContainerNode()) {
-                    transform(child, childJsonPointer);
+                    transform(child, childJsonPointer, usedPaths);
                 }
             }
         }
     }
 
+    private Path findUnusedPath(String jsonPointer, Set<Path> usedPaths) {
+        String filename = filenameReplacePattern.matcher(jsonPointer.substring(1)).replaceAll("_");
+        Path path = directory.resolve(filename + extension);
+        for (int i = 1; usedPaths.contains(path); i++) {
+            path = directory.resolve(filename + '-' + i + extension);
+        }
+        usedPaths.add(path);
+        return path;
+    }
+
     public static class Parameters {
-        public String directory;
+        public String directory = "";
+        public String filenameReplacePattern = "[\\\\/|\"'.,:;*?!<>{}\\s\\p{Cc}]";
         public String extension = "";
-        public String relativeTo;
         public StandardOpenOption[] openOptions = new StandardOpenOption[] { StandardOpenOption.CREATE_NEW };
+        public String pathRelativeTo;
     }
 }
