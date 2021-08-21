7aedfce8e95f3ee03f660f87309280ead25fd457
Binokkio <binokkio@b.nana.technology>
2021-08-21 17:35:38 +0200

    Elasticsearch.Write support for basic auth and multiple hosts

--- transformers/elasticsearch7/src/main/java/b/nana/technology/gingester/transformers/elasticsearch7/common/ElasticsearchTransformer.java
+++ transformers/elasticsearch7/src/main/java/b/nana/technology/gingester/transformers/elasticsearch7/common/ElasticsearchTransformer.java
@@ -2,29 +2,55 @@ package b.nana.technology.gingester.transformers.elasticsearch7.common;
 
 import b.nana.technology.gingester.core.Transformer;
 import com.fasterxml.jackson.annotation.JsonCreator;
+import org.apache.http.Header;
 import org.apache.http.HttpHost;
+import org.apache.http.message.BasicHeader;
 import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Collections;
+import java.util.List;
 
 public abstract class ElasticsearchTransformer<I, O> extends Transformer<I, O> {
 
-    private final String host;
-    private final int port;
-    private final String schema;
+    private final HttpHost[] hosts;
+    private final String authorization;
 
     protected RestHighLevelClient restClient;
 
     public ElasticsearchTransformer(Parameters parameters) {
         super(parameters);
-        host = parameters.host;
-        port = parameters.port;
-        schema = parameters.schema;
+
+        hosts = new HttpHost[parameters.hosts.size()];
+        for (int i = 0; i < parameters.hosts.size(); i++) {
+            hosts[i] = new HttpHost(
+                    parameters.hosts.get(i).host,
+                    parameters.hosts.get(i).port,
+                    parameters.hosts.get(i).schema
+            );
+        }
+
+        authorization = parameters.username != null ?
+                Base64.getEncoder().encodeToString((parameters.username + ':' + parameters.password).getBytes(StandardCharsets.UTF_8)) :
+                null;
     }
 
     @Override
     protected void open() {
-        restClient = new RestHighLevelClient(RestClient.builder(
-                new HttpHost(host, port, schema)));
+
+        RestClientBuilder builder = RestClient.builder(hosts);
+
+        if (authorization != null) {
+            builder.setDefaultHeaders(new Header[]{new BasicHeader(
+                    "Authorization",
+                    authorization
+            )});
+        }
+
+        restClient = new RestHighLevelClient(builder);
     }
 
     @Override
@@ -34,11 +60,17 @@ public abstract class ElasticsearchTransformer<I, O> extends Transformer<I, O> {
 
     public static class Parameters {
 
-        public String schema = "http";
-        public String host = "localhost";
-        public int port = 9200;
+        public List<Host> hosts = Collections.singletonList(new Host());
+        public String username;
+        public String password;
 
         @JsonCreator
         public Parameters() {}
+
+        public static class Host {
+            public String schema = "http";
+            public String host = "localhost";
+            public int port = 9200;
+        }
     }
 }
