5bd7d21cbe272c4cd5dfff01c5f5a247ab30b11a
Binokkio <binokkio@b.nana.technology>
2021-09-07 07:23:49 +0200

    Added -b -f -s -w CLI switches

--- core/src/main/java/b/nana/technology/gingester/core/main/Main.java
+++ core/src/main/java/b/nana/technology/gingester/core/main/Main.java
@@ -17,6 +17,7 @@ public final class Main {
 
     public static void main(String[] args) {
 
+        boolean break_ = false;
         boolean printConfig = false;
 
         Configuration configuration = new Configuration();
@@ -28,8 +29,14 @@ public final class Main {
 
             boolean markSyncFrom = false;
             boolean syncTo = false;
+            boolean fsw = false;
 
             switch (args[i]) {
+
+                case "-b":
+                case "--break":
+                    break_ = true;
+                    break;
 
                 case "-pc":
                 case "--print-config":
@@ -60,21 +67,32 @@ public final class Main {
                 case "-stt":
                 case "--sync-to-transformer":
                     syncTo = !markSyncFrom;  // bit of trickery to basically skip this case if we fell through the -sft case
+
+                case "-f":
+                case "--fetch":
+                case "-s":
+                case "--stash":
+                case "-w":
+                case "--swap":
+                    fsw = !markSyncFrom && !syncTo;  // same bit of trickery as above
                 case "-t":
                 case "--transformer":
 
-                    // TODO -s for -t Stash and -f for -t Fetch
-
                     b.nana.technology.gingester.core.controller.Configuration controller = new b.nana.technology.gingester.core.controller.Configuration();
 
-                    String next = args[++i];
-
-                    if (next.matches("\\d+")) {
-                        controller.async(true);
-                        controller.maxWorkers(Integer.parseInt(next));
-                        controller.transformer(args[++i]);
+                    if (fsw) {
+                        if (args[i].contains("f")) controller.transformer("Fetch");
+                        else if (args[i].contains("w")) controller.transformer("Swap");
+                        else controller.transformer("Stash");
                     } else {
-                        controller.transformer(next);
+                        String next = args[++i];
+                        if (next.matches("\\d+")) {
+                            controller.async(true);
+                            controller.maxWorkers(Integer.parseInt(next));
+                            controller.transformer(args[++i]);
+                        } else {
+                            controller.transformer(next);
+                        }
                     }
 
                     if (args.length > i + 1 && !args[i + 1].startsWith("-")) {
@@ -90,6 +108,8 @@ public final class Main {
                     } else if (syncTo) {
                         controller.syncs(List.of(syncFrom));
                     }
+
+                    if (break_) break;
 
                     configuration.transformers.add(controller);
 
