91859bf2083b6419029ee257041e8ced11686c50
Binokkio <binokkio@b.nana.technology>
2021-09-13 07:43:33 +0200

    Added Description and Pure annotations, but no handling

---
+++ core/src/main/java/b/nana/technology/gingester/core/annotations/Description.java
@@ -0,0 +1,9 @@
+package b.nana.technology.gingester.core.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Description {
+    String value();
+}
