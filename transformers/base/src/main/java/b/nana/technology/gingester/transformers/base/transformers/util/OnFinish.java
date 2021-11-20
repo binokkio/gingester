f325dc6e32ab86cf75f758ac12009ce0746f275f
Binokkio <binokkio@b.nana.technology>
2021-11-20 14:34:55 +0100

    Added default syncs for OnPrepare and OnFinish

--- transformers/base/src/main/java/b/nana/technology/gingester/transformers/base/transformers/util/OnFinish.java
+++ transformers/base/src/main/java/b/nana/technology/gingester/transformers/base/transformers/util/OnFinish.java
@@ -1,12 +1,20 @@
 package b.nana.technology.gingester.transformers.base.transformers.util;
 
 import b.nana.technology.gingester.core.annotations.Names;
+import b.nana.technology.gingester.core.configuration.SetupControls;
 import b.nana.technology.gingester.core.controller.Context;
 import b.nana.technology.gingester.core.receiver.Receiver;
 import b.nana.technology.gingester.core.transformer.Transformer;
 
+import java.util.Collections;
+
 @Names(1)
 public final class OnFinish implements Transformer<Object, Void> {
+
+    @Override
+    public void setup(SetupControls controls) {
+        controls.syncs(Collections.singletonList("__seed__"));
+    }
 
     @Override
     public void transform(Context context, Object in, Receiver<Void> out) throws Exception {
