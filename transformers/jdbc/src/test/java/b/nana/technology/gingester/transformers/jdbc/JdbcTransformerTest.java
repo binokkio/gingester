7a2043b3209ec4095fed97e2a8114c599a50a260
Binokkio <binokkio@b.nana.technology>
2021-12-06 21:42:30 +0100

    Improved Jdbc.Dql

--- transformers/jdbc/src/test/java/b/nana/technology/gingester/transformers/jdbc/JdbcTransformerTest.java
+++ transformers/jdbc/src/test/java/b/nana/technology/gingester/transformers/jdbc/JdbcTransformerTest.java
@@ -27,17 +27,45 @@ class JdbcTransformerTest {
         configuration.applyTo(writer);
         writer.run();
 
-        AtomicReference<Map<String, Object>> result = new AtomicReference<>();
+        AtomicReference<Map<String, Map<String, ?>>> result = new AtomicReference<>();
         Gingester reader = new Gingester();
         reader.cli("-t Jdbc.Dql \"{url:'jdbc:sqlite:" + tempFile + "',dql:'SELECT * FROM test'}\"");
         reader.add(result::set);
         reader.run();
 
-        Map<String, Object> container = (Map<String, Object>) result.get().get("test");
+        Map<String, ?> container = result.get().get("test");
         assertEquals(123, container.get("a"));
         assertEquals("Hello, World!", container.get("b"));
         assertEquals(true, container.get("c"));
 
         Files.delete(tempFile);
+    }
+
+    @Test
+    void testValueAndColumnNameInterpretation() {
+
+        AtomicReference<Map<String, Map<String, ?>>> result = new AtomicReference<>();
+
+        Gingester gingester = new Gingester();
+
+        gingester.cli("" +
+                "-t Json.Create \"{a:123,b:true,c:'Hello, World!'}\" " +
+                "-s in " +
+                "-t Jdbc.Dml \"{" +
+                "   ddl:'CREATE TABLE test (a INTEGER, b BOOLEAN, c TEXT)'," +
+                "   dml:{statement:'INSERT INTO test VALUES (?, ?, ?)',parameters:['in/a','in/b','in/c']}" +
+                "}\" " +
+                "-t OnFinish " +
+                "-t Jdbc.Dql 'SELECT *, a * 2 as a2, a * 3 as \"test.a3\" FROM test'");
+
+        gingester.add(result::set);
+
+        gingester.run();
+
+        assertEquals(246, result.get().get("__calculated__").get("a2"));
+        assertEquals(123, result.get().get("test").get("a"));
+        assertEquals(true, result.get().get("test").get("b"));
+        assertEquals("Hello, World!", result.get().get("test").get("c"));
+        assertEquals(369, result.get().get("test").get("a3"));
     }
 }
