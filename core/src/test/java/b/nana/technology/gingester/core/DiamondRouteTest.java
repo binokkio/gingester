8bdf6389a249f96df5484f59d53c3d7cd255631e
Binokkio <binokkio@b.nana.technology>
2021-12-25 09:44:01 +0100

    Added attach to target tests

--- core/src/test/java/b/nana/technology/gingester/core/DiamondRouteTest.java
+++ core/src/test/java/b/nana/technology/gingester/core/DiamondRouteTest.java
@@ -21,4 +21,35 @@ class DiamondRouteTest {
         assertEquals("Hello, World!", results.remove());
         assertEquals("Hello, World?", results.remove());
     }
+
+    @Test
+    void testAttachToTarget() {
+
+        ArrayDeque<String> results = new ArrayDeque<>();
+
+        Gingester gingester = new Gingester("-cr hello-world-diamond.cli");
+        gingester.attach(results::add, "Emphasize");
+        gingester.run();
+
+        assertEquals(1, results.size());
+        assertEquals("Hello, World!", results.remove());
+    }
+
+    @Test
+    void testAttachToTargets() {
+
+        ArrayDeque<String> emphasizeResults = new ArrayDeque<>();
+        ArrayDeque<String> questionResults = new ArrayDeque<>();
+
+        Gingester gingester = new Gingester("-cr hello-world-diamond.cli");
+        gingester.attach(emphasizeResults::add, "Emphasize");
+        gingester.attach(questionResults::add, "Question");
+        gingester.run();
+
+        assertEquals(1, emphasizeResults.size());
+        assertEquals("Hello, World!", emphasizeResults.remove());
+
+        assertEquals(1, questionResults.size());
+        assertEquals("Hello, World?", questionResults.remove());
+    }
 }
