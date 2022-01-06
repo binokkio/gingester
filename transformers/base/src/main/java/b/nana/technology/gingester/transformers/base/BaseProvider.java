5115c5b21a23d343fc107bedfa29ea8affe82505
Binokkio <binokkio@b.nana.technology>
2022-01-07 00:14:34 +0100

    Added InputStreamSplitLines based on the BufferedReader

--- transformers/base/src/main/java/b/nana/technology/gingester/transformers/base/BaseProvider.java
+++ transformers/base/src/main/java/b/nana/technology/gingester/transformers/base/BaseProvider.java
@@ -48,6 +48,7 @@ public final class BaseProvider implements Provider {
                 b.nana.technology.gingester.transformers.base.transformers.inputstream.Prepend.class,
                 b.nana.technology.gingester.transformers.base.transformers.inputstream.Skip.class,
                 b.nana.technology.gingester.transformers.base.transformers.inputstream.Split.class,
+                b.nana.technology.gingester.transformers.base.transformers.inputstream.SplitLines.class,
                 b.nana.technology.gingester.transformers.base.transformers.inputstream.ToBytes.class,
                 b.nana.technology.gingester.transformers.base.transformers.inputstream.ToJson.class,
                 b.nana.technology.gingester.transformers.base.transformers.inputstream.ToOutputStream.class,
