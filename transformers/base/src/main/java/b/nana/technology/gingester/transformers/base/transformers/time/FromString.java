061a089934fc64b067c8742836694ae346ca0066
Binokkio <binokkio@b.nana.technology>
2022-02-09 19:22:54 +0100

    Fixed TimeFromString bug where zone parameter was ignored, unit test

--- transformers/base/src/main/java/b/nana/technology/gingester/transformers/base/transformers/time/FromString.java
+++ transformers/base/src/main/java/b/nana/technology/gingester/transformers/base/transformers/time/FromString.java
@@ -7,10 +7,7 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
-import java.time.temporal.ChronoField;
-import java.time.temporal.IsoFields;
-import java.time.temporal.TemporalAccessor;
-import java.time.temporal.TemporalField;
+import java.time.temporal.*;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -24,13 +21,17 @@ public final class FromString extends TimeBase<String, TemporalAccessor> {
 
         DateTimeFormatterBuilder builder = getFormatBuilder();
 
-        parameters.defaults.forEach((letter, value) ->
-                builder.parseDefaulting(FIELD_MAP.get(letter), value));
+        parameters.defaults.forEach((key, value) -> {
+            TemporalField temporalField = FIELD_MAP.get(key);
+            if (temporalField == null) throw new IllegalArgumentException("Unknown default: " + key);
+            builder.parseDefaulting(temporalField, value);
+        });
 
         DateTimeFormatter formatter = builder.toFormatter();
 
-        if (parameters.zone != null) {
-            formatter.withZone(ZoneId.of(parameters.zone));
+        ZoneId zoneId = getZoneId().orElse(null);
+        if (zoneId != null) {
+            formatter = formatter.withZone(zoneId);
         }
 
         this.formatter = formatter;
@@ -43,7 +44,7 @@ public final class FromString extends TimeBase<String, TemporalAccessor> {
 
     public static class Parameters extends TimeBase.Parameters {
 
-        public Map<Character, Integer> defaults = Collections.emptyMap();
+        public Map<String, Integer> defaults = Collections.emptyMap();
 
         @JsonCreator
         public Parameters() {}
@@ -54,31 +55,45 @@ public final class FromString extends TimeBase<String, TemporalAccessor> {
         }
     }
 
-    private static final Map<Character, TemporalField> FIELD_MAP = new HashMap<>();
+    private static final Map<String, TemporalField> FIELD_MAP = new HashMap<>();
     static {
-        FIELD_MAP.put('G', ChronoField.ERA);
-        FIELD_MAP.put('y', ChronoField.YEAR_OF_ERA);
-        FIELD_MAP.put('u', ChronoField.YEAR);
-        FIELD_MAP.put('Q', IsoFields.QUARTER_OF_YEAR);
-        FIELD_MAP.put('q', IsoFields.QUARTER_OF_YEAR);
-        FIELD_MAP.put('M', ChronoField.MONTH_OF_YEAR);
-        FIELD_MAP.put('L', ChronoField.MONTH_OF_YEAR);
-        FIELD_MAP.put('D', ChronoField.DAY_OF_YEAR);
-        FIELD_MAP.put('d', ChronoField.DAY_OF_MONTH);
-        FIELD_MAP.put('F', ChronoField.ALIGNED_DAY_OF_WEEK_IN_MONTH);
-        FIELD_MAP.put('E', ChronoField.DAY_OF_WEEK);
-        FIELD_MAP.put('c', ChronoField.DAY_OF_WEEK);
-        FIELD_MAP.put('e', ChronoField.DAY_OF_WEEK);
-        FIELD_MAP.put('a', ChronoField.AMPM_OF_DAY);
-        FIELD_MAP.put('H', ChronoField.HOUR_OF_DAY);
-        FIELD_MAP.put('k', ChronoField.CLOCK_HOUR_OF_DAY);
-        FIELD_MAP.put('K', ChronoField.HOUR_OF_AMPM);
-        FIELD_MAP.put('h', ChronoField.CLOCK_HOUR_OF_AMPM);
-        FIELD_MAP.put('m', ChronoField.MINUTE_OF_HOUR);
-        FIELD_MAP.put('s', ChronoField.SECOND_OF_MINUTE);
-        FIELD_MAP.put('S', ChronoField.NANO_OF_SECOND);
-        FIELD_MAP.put('A', ChronoField.MILLI_OF_DAY);
-        FIELD_MAP.put('n', ChronoField.NANO_OF_SECOND);
-        FIELD_MAP.put('N', ChronoField.NANO_OF_DAY);
+        FIELD_MAP.put("alignedDayOfWeekInMonth", ChronoField.ALIGNED_DAY_OF_WEEK_IN_MONTH);
+        FIELD_MAP.put("alignedDayOfWeekInYear", ChronoField.ALIGNED_DAY_OF_WEEK_IN_YEAR);
+        FIELD_MAP.put("alignedWeekOfMonth", ChronoField.ALIGNED_WEEK_OF_MONTH);
+        FIELD_MAP.put("alignedWeekOfYear", ChronoField.ALIGNED_WEEK_OF_YEAR);
+        FIELD_MAP.put("amPmOfDay", ChronoField.AMPM_OF_DAY);
+        FIELD_MAP.put("clockHourOfAmPm", ChronoField.CLOCK_HOUR_OF_AMPM);
+        FIELD_MAP.put("clockHourOfDay", ChronoField.CLOCK_HOUR_OF_DAY);
+        FIELD_MAP.put("dayOfMonth", ChronoField.DAY_OF_MONTH);
+        FIELD_MAP.put("dayOfQuarter", IsoFields.DAY_OF_QUARTER);
+        FIELD_MAP.put("dayOfWeek", ChronoField.DAY_OF_WEEK);
+        FIELD_MAP.put("dayOfYear", ChronoField.DAY_OF_YEAR);
+        FIELD_MAP.put("epochDay", ChronoField.EPOCH_DAY);
+        FIELD_MAP.put("era", ChronoField.ERA);
+        FIELD_MAP.put("hourOfAmPm", ChronoField.HOUR_OF_AMPM);
+        FIELD_MAP.put("hourOfDay", ChronoField.HOUR_OF_DAY);
+        FIELD_MAP.put("instantSeconds", ChronoField.INSTANT_SECONDS);
+        FIELD_MAP.put("julianDay", JulianFields.JULIAN_DAY);
+        FIELD_MAP.put("microOfDay", ChronoField.MICRO_OF_DAY);
+        FIELD_MAP.put("microOfSecond", ChronoField.MICRO_OF_SECOND);
+        FIELD_MAP.put("milliOfDay", ChronoField.MILLI_OF_DAY);
+        FIELD_MAP.put("milliOfSecond", ChronoField.MILLI_OF_SECOND);
+        FIELD_MAP.put("minuteOfDay", ChronoField.MINUTE_OF_DAY);
+        FIELD_MAP.put("minuteOfHour", ChronoField.MINUTE_OF_HOUR);
+        FIELD_MAP.put("modifiedJulianDay", JulianFields.MODIFIED_JULIAN_DAY);
+        FIELD_MAP.put("monthOfYear", ChronoField.MONTH_OF_YEAR);
+        FIELD_MAP.put("nanoOfDay", ChronoField.NANO_OF_DAY);
+        FIELD_MAP.put("nanoOfSecond", ChronoField.NANO_OF_SECOND);
+        FIELD_MAP.put("offsetSeconds", ChronoField.OFFSET_SECONDS);
+        FIELD_MAP.put("prolepticMonth", ChronoField.PROLEPTIC_MONTH);
+        FIELD_MAP.put("quarterOfYear", IsoFields.QUARTER_OF_YEAR);
+        FIELD_MAP.put("rataDie", JulianFields.RATA_DIE);
+        FIELD_MAP.put("secondOfDay", ChronoField.SECOND_OF_DAY);
+        FIELD_MAP.put("secondOfMinute", ChronoField.SECOND_OF_MINUTE);
+        FIELD_MAP.put("weekBasedYear", IsoFields.WEEK_BASED_YEAR);
+        FIELD_MAP.put("weekOfWeekBasedYear", IsoFields.WEEK_OF_WEEK_BASED_YEAR);
+        FIELD_MAP.put("year", ChronoField.YEAR);
+        FIELD_MAP.put("yearOfEra", ChronoField.YEAR_OF_ERA);
+
     }
 }
---
+++ transformers/base/src/test/java/b/nana/technology/gingester/transformers/base/transformers/time/FromStringTest.java
@@ -0,0 +1,46 @@
+package b.nana.technology.gingester.transformers.base.transformers.time;
+
+import b.nana.technology.gingester.core.receiver.UniReceiver;
+import org.junit.jupiter.api.Test;
+
+import java.time.ZonedDateTime;
+import java.time.temporal.TemporalAccessor;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class FromStringTest {
+
+    @Test
+    void testDefaults() throws Exception {
+
+        FromString.Parameters parameters = new FromString.Parameters();
+        parameters.format = "Q-yyyy";
+        parameters.defaults = Map.of(
+            "dayOfQuarter", 10,
+            "nanoOfDay", 0
+        );
+        parameters.zone = "UTC";
+
+        AtomicReference<TemporalAccessor> result = new AtomicReference<>();
+        new FromString(parameters).transform(null, "2-2022", (UniReceiver<TemporalAccessor>) result::set);
+
+        assertEquals(ZonedDateTime.parse("2022-04-10T00:00Z[UTC]"), ZonedDateTime.from(result.get()));
+    }
+
+    @Test
+    void testUnknownDefaultThrows() {
+
+        FromString.Parameters parameters = new FromString.Parameters();
+        parameters.format = "Q-yyyy";
+        parameters.defaults = Map.of(
+            "unknownDefault", 123
+        );
+        parameters.zone = "UTC";
+
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> new FromString(parameters));
+        assertEquals("Unknown default: unknownDefault", e.getMessage());
+    }
+}
