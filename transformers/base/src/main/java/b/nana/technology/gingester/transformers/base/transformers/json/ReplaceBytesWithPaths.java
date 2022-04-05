eea714c24ec7e89ab86e354bd386fd796033af71
Binokkio <binokkio@b.nana.technology>
2022-04-05 22:52:11 +0200

    JsonReplaceBytesWithPaths convenience and examples

--- transformers/base/src/main/java/b/nana/technology/gingester/transformers/base/transformers/json/ReplaceBytesWithPaths.java
+++ transformers/base/src/main/java/b/nana/technology/gingester/transformers/base/transformers/json/ReplaceBytesWithPaths.java
@@ -1,11 +1,14 @@
 package b.nana.technology.gingester.transformers.base.transformers.json;
 
+import b.nana.technology.gingester.core.annotations.Example;
+import b.nana.technology.gingester.core.configuration.NormalizingDeserializer;
 import b.nana.technology.gingester.core.controller.Context;
 import b.nana.technology.gingester.core.receiver.Receiver;
-import b.nana.technology.gingester.core.template.Template;
+import b.nana.technology.gingester.core.template.TemplateMapper;
 import b.nana.technology.gingester.core.template.TemplateParameters;
 import b.nana.technology.gingester.core.transformer.Transformer;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.BinaryNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
@@ -18,17 +21,19 @@ import java.util.Iterator;
 import java.util.Set;
 import java.util.regex.Pattern;
 
+@Example(example = "'/tmp/assets'", description = "Write byte arrays to files in /tmp/assets and put the resulting paths in the json")
+@Example(example = "'{directory: \"/tmp/assets\", pathsRelativeTo: \"/tmp\"}'", description = "Same as above but with paths relative to /tmp")
 public final class ReplaceBytesWithPaths implements Transformer<JsonNode, JsonNode> {
 
-    private final Template directoryTemplate;
-    private final Template pathsRelativeToTemplate;
+    private final TemplateMapper<Path> directoryTemplate;
+    private final TemplateMapper<Path> pathsRelativeToTemplate;
     private final Pattern filenameReplacePattern;
     private final String extension;
     private final OpenOption[] openOptions;
 
     public ReplaceBytesWithPaths(Parameters parameters) {
-        directoryTemplate = Context.newTemplate(parameters.directory);
-        pathsRelativeToTemplate = Context.newTemplate(parameters.pathsRelativeTo != null ? parameters.pathsRelativeTo : parameters.directory);
+        directoryTemplate = Context.newTemplateMapper(parameters.directory, Paths::get);
+        pathsRelativeToTemplate = parameters.pathsRelativeTo != null ? Context.newTemplateMapper(parameters.pathsRelativeTo, Paths::get) : null;
         filenameReplacePattern = Pattern.compile(parameters.filenameReplacePattern);
         extension = parameters.extension;
         openOptions = parameters.openOptions;
@@ -41,7 +46,8 @@ public final class ReplaceBytesWithPaths implements Transformer<JsonNode, JsonNo
     }
 
     private void transform(Context context, JsonNode jsonNode, String jsonPointer, Set<Path> usedPaths) throws IOException {
-        Path pathRelativeTo = Paths.get(pathsRelativeToTemplate.render(Context.newTestContext()));
+        Path directory = directoryTemplate.render(context);
+        Path pathRelativeTo = pathsRelativeToTemplate != null ? pathsRelativeToTemplate.render(context) : null;
         if (jsonNode.isObject()) {
             Iterator<String> keys = jsonNode.fieldNames();
             while (keys.hasNext()) {
@@ -49,9 +55,9 @@ public final class ReplaceBytesWithPaths implements Transformer<JsonNode, JsonNo
                 String childJsonPointer = jsonPointer + "/" + key;
                 JsonNode child = jsonNode.get(key);
                 if (child.isBinary()) {
-                    Path path = findUnusedPath(context, childJsonPointer, usedPaths);
+                    Path path = findUnusedPath(directory, childJsonPointer, usedPaths);
                     Files.write(path, ((BinaryNode) child).binaryValue(), openOptions);
-                    ((ObjectNode) jsonNode).put(key, pathRelativeTo.relativize(path).toString());
+                    ((ObjectNode) jsonNode).put(key, getReplacement(path, pathRelativeTo));
                 } else if (child.isContainerNode()) {
                     transform(context, child, childJsonPointer, usedPaths);
                 }
@@ -61,9 +67,9 @@ public final class ReplaceBytesWithPaths implements Transformer<JsonNode, JsonNo
                 String childJsonPointer = jsonPointer + "/" + i;
                 JsonNode child = jsonNode.get(i);
                 if (child.isBinary()) {
-                    Path path = findUnusedPath(context, childJsonPointer, usedPaths);
+                    Path path = findUnusedPath(directory, childJsonPointer, usedPaths);
                     Files.write(path, ((BinaryNode) child).binaryValue(), openOptions);
-                    ((ArrayNode) jsonNode).set(i, ((ArrayNode) jsonNode).textNode(pathRelativeTo.relativize(path).toString()));
+                    ((ArrayNode) jsonNode).set(i, ((ArrayNode) jsonNode).textNode(getReplacement(path, pathRelativeTo)));
                 } else if (child.isContainerNode()) {
                     transform(context, child, childJsonPointer, usedPaths);
                 }
@@ -71,9 +77,8 @@ public final class ReplaceBytesWithPaths implements Transformer<JsonNode, JsonNo
         }
     }
 
-    private Path findUnusedPath(Context context, String jsonPointer, Set<Path> usedPaths) throws IOException {
+    private Path findUnusedPath(Path directory, String jsonPointer, Set<Path> usedPaths) throws IOException {
         String filename = filenameReplacePattern.matcher(jsonPointer.substring(1)).replaceAll("_");
-        Path directory = Paths.get(directoryTemplate.render(context));
         Files.createDirectories(directory);
         Path path = directory.resolve(filename + extension);
         for (int i = 1; usedPaths.contains(path); i++) {
@@ -83,7 +88,25 @@ public final class ReplaceBytesWithPaths implements Transformer<JsonNode, JsonNo
         return path;
     }
 
+    private String getReplacement(Path path, Path pathRelativeTo) {
+        if (pathRelativeTo != null) {
+            return pathRelativeTo.relativize(path).toString();
+        } else {
+            return path.toString();
+        }
+    }
+
+    @JsonDeserialize(using = Parameters.Deserializer.class)
     public static class Parameters {
+
+        public static class Deserializer extends NormalizingDeserializer<Parameters> {
+            public Deserializer() {
+                super(Parameters.class);
+                rule(JsonNode::isTextual, text -> o("directory", text));
+                rule(json -> json.has("template"), json -> o("directory", json));
+            }
+        }
+
         public TemplateParameters directory = new TemplateParameters("", true);
         public TemplateParameters pathsRelativeTo;
         public String filenameReplacePattern = "[\\\\/|\"'.,:;#*?!<>\\[\\]{}\\s\\p{Cc}]";
