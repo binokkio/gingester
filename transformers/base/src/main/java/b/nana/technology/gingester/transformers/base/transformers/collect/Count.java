ecf5f9b09149ad2f9093fabca448ea6f7569c9c7
Binokkio <binokkio@b.nana.technology>
2022-05-10 07:58:01 +0200

    CollectCount transformer

---
+++ transformers/base/src/main/java/b/nana/technology/gingester/transformers/base/transformers/collect/Count.java
@@ -0,0 +1,26 @@
+package b.nana.technology.gingester.transformers.base.transformers.collect;
+
+import b.nana.technology.gingester.core.controller.Context;
+import b.nana.technology.gingester.core.controller.ContextMap;
+import b.nana.technology.gingester.core.receiver.Receiver;
+import b.nana.technology.gingester.core.transformer.Transformer;
+
+public class Count implements Transformer<Object, Long> {
+
+    private final ContextMap<long[]> counters = new ContextMap<>();
+
+    @Override
+    public void prepare(Context context, Receiver<Long> out) throws Exception {
+        counters.put(context, new long[1]);
+    }
+
+    @Override
+    public void transform(Context context, Object in, Receiver<Long> out) throws Exception {
+        counters.act(context, counter -> counter[0]++);
+    }
+
+    @Override
+    public void finish(Context context, Receiver<Long> out) throws Exception {
+        out.accept(context, counters.remove(context)[0]);
+    }
+}
---
+++ transformers/base/src/test/java/b/nana/technology/gingester/transformers/base/transformers/collect/CountTest.java
@@ -0,0 +1,28 @@
+package b.nana.technology.gingester.transformers.base.transformers.collect;
+
+import b.nana.technology.gingester.core.Gingester;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayDeque;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CountTest {
+
+    @Test
+    void test() {
+
+        ArrayDeque<Long> result = new ArrayDeque<>();
+
+        new Gingester("" +
+                "-sft Repeat 2 " +
+                "-t Repeat 1000 " +
+                "-stt CollectCount")
+                .attach(result::add)
+                .run();
+
+        assertEquals(2, result.size());
+        assertEquals(1000, result.remove());
+        assertEquals(1000, result.remove());
+    }
+}
