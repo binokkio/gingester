9aa82b5d2007306abf7c15f42b3b3066444c7e4d
Binokkio <binokkio@b.nana.technology>
2022-05-11 07:39:31 +0200

    Groovy yield shorthand

--- transformers/groovy/src/main/java/b/nana/technology/gingester/transformers/groovy/Groovy.java
+++ transformers/groovy/src/main/java/b/nana/technology/gingester/transformers/groovy/Groovy.java
@@ -7,32 +7,58 @@ import b.nana.technology.gingester.core.template.TemplateParameters;
 import b.nana.technology.gingester.core.transformer.Transformer;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import groovy.lang.Binding;
+import groovy.lang.Closure;
 import groovy.lang.GroovyShell;
 import groovy.lang.Script;
 
 public final class Groovy implements Transformer<Object, Object> {
 
-    private final ThreadLocal<TemplateMapper<Script>> scripts;
+    private final ThreadLocal<TemplateMapper<ScriptWithYield>> scripts;
 
     public Groovy(Parameters parameters) {
         scripts = ThreadLocal.withInitial(() -> Context.newTemplateMapper(
                 parameters.script,
-                script -> new GroovyShell().parse(script)
+                script -> new ScriptWithYield(new GroovyShell().parse(script))
         ));
     }
 
     @Override
     public void transform(Context context, Object in, Receiver<Object> out) {
 
+        ScriptWithYield script = scripts.get().render(context);
+        script.context = context;
+        script.out = out;
+
         Binding binding = new Binding();
         binding.setProperty("context", context);
         binding.setProperty("in", in);
         binding.setProperty("out", out);
+        binding.setProperty("yield", script.yield);
 
-        Script script = scripts.get().render(context);
-        script.setBinding(binding);
-        script.run();
-        script.setBinding(null);
+        script.script.setBinding(binding);
+        script.script.run();
+        script.script.setBinding(null);
+        script.context = null;
+        script.out = null;
+    }
+
+    private final static class ScriptWithYield {
+
+        private final Script script;
+        private final Closure<Void> yield = new Closure<>(this) {
+            @Override
+            public Void call(Object... args) {
+                out.accept(context, args[0]);
+                return null;
+            }
+        };
+
+        private Context context;
+        private Receiver<Object> out;
+
+        private ScriptWithYield(Script script) {
+            this.script = script;
+        }
     }
 
     public static class Parameters {
--- transformers/groovy/src/test/java/b/nana/technology/gingester/transformers/groovy/GroovyTest.java
+++ transformers/groovy/src/test/java/b/nana/technology/gingester/transformers/groovy/GroovyTest.java
@@ -3,12 +3,33 @@ package b.nana.technology.gingester.transformers.groovy;
 import b.nana.technology.gingester.core.Gingester;
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.atomic.AtomicReference;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class GroovyTest {
 
     @Test
-    void test() {
-        new Gingester("" +
-                "-t Groovy 'out.accept(context, 3 * 3)' " +
-                "-t Out false").run();
+    void testYield() {
+
+        AtomicReference<Object> result = new AtomicReference<>();
+
+        new Gingester("-t Groovy 'yield(3 * 3)'")
+                .attach(result::set)
+                .run();
+
+        assertEquals(9, result.get());
+    }
+
+    @Test
+    void testOutAccept() {
+
+        AtomicReference<Object> result = new AtomicReference<>();
+
+        new Gingester("-t Groovy 'out.accept(context, 3 * 3)'")
+                .attach(result::set)
+                .run();
+
+        assertEquals(9, result.get());
     }
 }
