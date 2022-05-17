a5025bdc83f59218fb581099286e8120cfded406
Binokkio <binokkio@b.nana.technology>
2022-05-17 20:29:45 +0200

    XToJson transformers no consume their entire input and output multiple objects if necessary

---
+++ transformers/base/src/main/java/b/nana/technology/gingester/transformers/base/common/json/IteratingToJsonTransformer.java
@@ -0,0 +1,33 @@
+package b.nana.technology.gingester.transformers.base.common.json;
+
+import b.nana.technology.gingester.core.controller.Context;
+import b.nana.technology.gingester.core.receiver.Receiver;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.MappingIterator;
+import com.fasterxml.jackson.databind.ObjectReader;
+
+import java.io.IOException;
+
+public abstract class IteratingToJsonTransformer<T> extends ToJsonTransformer<T> {
+
+    protected IteratingToJsonTransformer(Parameters parameters) {
+        super(parameters);
+    }
+
+    @Override
+    public final void transform(Context context, T in, Receiver<JsonNode> out) throws Exception {
+        JsonParser parser = getJsonParser(getObjectReader(), in);
+        JsonToken token = parser.nextToken();
+        if (token != null) {
+            MappingIterator<JsonNode> iterator = getObjectReader().readValues(parser);
+            long counter = 0;
+            while (iterator.hasNext()) {
+                out.accept(context.stash("description", counter++), iterator.next());
+            }
+        }
+    }
+
+    protected abstract JsonParser getJsonParser(ObjectReader objectReader, T in) throws IOException;
+}
--- transformers/base/src/main/java/b/nana/technology/gingester/transformers/base/transformers/inputstream/ToJson.java
+++ transformers/base/src/main/java/b/nana/technology/gingester/transformers/base/transformers/inputstream/ToJson.java
@@ -1,22 +1,23 @@
 package b.nana.technology.gingester.transformers.base.transformers.inputstream;
 
 import b.nana.technology.gingester.core.annotations.Pure;
-import b.nana.technology.gingester.core.controller.Context;
-import b.nana.technology.gingester.core.receiver.Receiver;
+import b.nana.technology.gingester.transformers.base.common.json.IteratingToJsonTransformer;
 import b.nana.technology.gingester.transformers.base.common.json.ToJsonTransformer;
-import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.ObjectReader;
 
+import java.io.IOException;
 import java.io.InputStream;
 
 @Pure
-public final class ToJson extends ToJsonTransformer<InputStream> {
+public final class ToJson extends IteratingToJsonTransformer<InputStream> {
 
     public ToJson(ToJsonTransformer.Parameters parameters) {
         super(parameters);
     }
 
     @Override
-    public void transform(Context context, InputStream in, Receiver<JsonNode> out) throws Exception {
-        out.accept(context, getObjectReader().readTree(in));
+    protected JsonParser getJsonParser(ObjectReader objectReader, InputStream in) throws IOException {
+        return objectReader.createParser(in);
     }
 }
