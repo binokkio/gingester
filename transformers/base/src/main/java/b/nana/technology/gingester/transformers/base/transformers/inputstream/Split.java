be898cf07d8a0dc2db1c3f078726997296c35af8
Binokkio <binokkio@b.nana.technology>
2022-05-27 07:36:32 +0200

    Add InputStreamSplit maxSplits parameter

--- transformers/base/src/main/java/b/nana/technology/gingester/transformers/base/transformers/inputstream/Split.java
+++ transformers/base/src/main/java/b/nana/technology/gingester/transformers/base/transformers/inputstream/Split.java
@@ -1,11 +1,13 @@
 package b.nana.technology.gingester.transformers.base.transformers.inputstream;
 
+import b.nana.technology.gingester.core.configuration.NormalizingDeserializer;
 import b.nana.technology.gingester.core.configuration.SetupControls;
 import b.nana.technology.gingester.core.controller.Context;
 import b.nana.technology.gingester.core.receiver.Receiver;
 import b.nana.technology.gingester.core.transformer.Transformer;
 import b.nana.technology.gingester.transformers.base.common.iostream.Splitter;
-import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
@@ -14,9 +16,11 @@ import java.util.Optional;
 public final class Split implements Transformer<InputStream, InputStream> {
 
     private final byte[] delimiter;
+    private final long maxSplits;
 
     public Split(Parameters parameters) {
         delimiter = parameters.delimiter.getBytes(StandardCharsets.UTF_8);
+        maxSplits = parameters.maxSplits;
     }
 
     @Override
@@ -31,19 +35,26 @@ public final class Split implements Transformer<InputStream, InputStream> {
         long counter = 0;
         while ((split = splitter.getNextInputStream()).isPresent()) {
             out.accept(context.stash("description", counter++), split.get());
+            if (counter == maxSplits) {
+                out.accept(context.stash("description", "remaining"), splitter.getRemaining());
+                break;
+            }
         }
     }
 
+    @JsonDeserialize(using = Parameters.Deserializer.class)
     public static class Parameters {
 
-        public String delimiter = "\n";
-
-        @JsonCreator
-        public Parameters() {}
-
-        @JsonCreator
-        public Parameters(String delimiter) {
-            this.delimiter = delimiter;
+        public static class Deserializer extends NormalizingDeserializer<Parameters> {
+            public Deserializer() {
+                super(Parameters.class);
+                rule(JsonNode::isTextual, delimiter -> o("delimiter", delimiter));
+                rule(JsonNode::isNumber, maxSplits -> o("maxSplits", maxSplits));
+                rule(JsonNode::isArray, array -> o("delimiter", array.get(0), "maxSplits", array.get(1)));
+            }
         }
+
+        public String delimiter = "\n";
+        public long maxSplits;
     }
 }
