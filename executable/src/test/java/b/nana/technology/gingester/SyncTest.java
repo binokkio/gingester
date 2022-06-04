b41680847c9a3453488a2b53372e4195d27feb11
Binokkio <binokkio@b.nana.technology>
2022-06-04 18:29:09 +0200

    Fix join after sync deadlock

---
+++ executable/src/test/java/b/nana/technology/gingester/SyncTest.java
@@ -0,0 +1,19 @@
+package b.nana.technology.gingester;
+
+import b.nana.technology.gingester.core.Gingester;
+import org.junit.jupiter.api.Test;
+
+class SyncTest {
+
+    @Test
+    void testSyncBeforeJoin() {
+
+        new Gingester().cli("" +
+                "-t StringCreate 'Hello, World!' " +
+                "-sft Repeat 100000 " +
+                "-stt Passthrough " +
+                "-t InputStreamJoin " +
+                "-t InputStreamDrain")
+                .run();
+    }
+}
