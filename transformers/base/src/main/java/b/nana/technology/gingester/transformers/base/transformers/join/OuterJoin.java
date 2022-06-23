b5edf783fb52f406beb55388a699761f7fa2c4a1
Binokkio <binokkio@b.nana.technology>
2022-06-23 20:45:58 +0200

    Add OuterJoin transformer

---
+++ transformers/base/src/main/java/b/nana/technology/gingester/transformers/base/transformers/join/OuterJoin.java
@@ -0,0 +1,86 @@
+package b.nana.technology.gingester.transformers.base.transformers.join;
+
+import b.nana.technology.gingester.core.controller.Context;
+import b.nana.technology.gingester.core.controller.ContextMap;
+import b.nana.technology.gingester.core.controller.FetchKey;
+import b.nana.technology.gingester.core.receiver.Receiver;
+import b.nana.technology.gingester.core.transformer.Transformer;
+import org.antlr.v4.runtime.misc.Pair;
+
+import java.util.*;
+
+public final class OuterJoin implements Transformer<Object, Map<String, Object>> {
+
+    private static final FetchKey FETCH_JOIN_AS = new FetchKey("joinAs");
+    private static final FetchKey FETCH_VALUE = new FetchKey(1);
+
+    private final ContextMap<Map<String, Map<Object, List<Object>>>> states = new ContextMap<>();
+
+    private final Set<String> lists = Collections.emptySet();
+
+    @Override
+    public void prepare(Context context, Receiver<Map<String, Object>> out) throws Exception {
+        states.put(context, new HashMap<>());
+    }
+
+    @Override
+    public void transform(Context context, Object key, Receiver<Map<String, Object>> out) throws Exception {
+
+        String joinAs = (String) context.require(FETCH_JOIN_AS);
+        Object value = context.require(FETCH_VALUE);
+
+        states.act(context, state -> state
+                .computeIfAbsent(joinAs, x -> new HashMap<>())
+                .computeIfAbsent(key, x -> new ArrayList<>())
+                .add(value));
+    }
+
+    @Override
+    public void finish(Context context, Receiver<Map<String, Object>> out) throws Exception {
+
+        Map<String, Map<Object, List<Object>>> state = states.remove(context);
+        Set<Object> seen = new HashSet<>();
+
+        state.values().stream().map(Map::keySet).flatMap(Collection::stream).forEach(key -> {
+            if (!seen.contains(key)) {
+                seen.add(key);
+
+                List<Pair<String, List<Object>>> data = new ArrayList<>();
+                state.forEach((joinAs, joinData) ->
+                        data.add(new Pair<>(joinAs, joinData.getOrDefault(key,
+                                lists.contains(joinAs) ?
+                                        Collections.emptyList() :
+                                        Collections.singletonList(null)))));
+
+                permutate(data, 0, new HashMap<>(), context, out);
+            }
+        });
+    }
+
+    private void permutate(List<Pair<String, List<Object>>> data, int depth, Map<String, Object> result, Context context, Receiver<Map<String, Object>> out) {
+
+        boolean atMaxDepth = depth == data.size() - 1;
+
+        Pair<String, List<Object>> pair = data.get(depth);
+        String joinAs = pair.a;
+        List<Object> values = pair.b;
+
+        if (lists.contains(joinAs)) {
+            result.put(joinAs, pair.b);
+            if (atMaxDepth) {
+                out.accept(context, new HashMap<>(result));
+            } else {
+                permutate(data, depth + 1, result, context, out);
+            }
+        } else {
+            for (Object value : values) {
+                result.put(joinAs, value);
+                if (atMaxDepth) {
+                    out.accept(context, new HashMap<>(result));
+                } else {
+                    permutate(data, depth + 1, result, context, out);
+                }
+            }
+        }
+    }
+}
---
+++ transformers/base/src/test/java/b/nana/technology/gingester/transformers/base/transformers/join/OuterJoinTest.java
@@ -0,0 +1,130 @@
+package b.nana.technology.gingester.transformers.base.transformers.join;
+
+import b.nana.technology.gingester.core.Gingester;
+import com.fasterxml.jackson.databind.JsonNode;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OuterJoinTest {
+
+    @Test
+    void testOneToOneJoin() {
+
+        Deque<Map<String, JsonNode>> result = new ArrayDeque<>();
+
+        new Gingester().cli("" +
+                "-ss joinAs numbers -t JsonCreate [{id:1,value:11111},{id:2,value:22222},{id:3,value:33333}] -l JsonStream " +
+                "-ss joinAs letters -t JsonCreate [{id:1,value:'aaa'},{id:2,value:'bbb'},{id:3,value:'ccc'}] -l JsonStream " +
+                "-t JsonStream '$[*]' " +
+                "-s value " +
+                "-t JsonPath '$.id' " +
+                "-t OuterJoin")
+                .attach(result::add)
+                .run();
+
+        List<Map<String, JsonNode>> sorted = result.stream()
+                .sorted(Comparator.comparing(i -> i.get("numbers").get("id").asInt()))
+                .collect(Collectors.toList());
+
+        assertEquals(1, sorted.get(0).get("numbers").get("id").asInt());
+        assertEquals(1, sorted.get(0).get("letters").get("id").asInt());
+        assertEquals(11111, sorted.get(0).get("numbers").get("value").asInt());
+        assertEquals("aaa", sorted.get(0).get("letters").get("value").asText());
+
+        assertEquals(2, sorted.get(1).get("numbers").get("id").asInt());
+        assertEquals(2, sorted.get(1).get("letters").get("id").asInt());
+        assertEquals(22222, sorted.get(1).get("numbers").get("value").asInt());
+        assertEquals("bbb", sorted.get(1).get("letters").get("value").asText());
+
+        assertEquals(3, sorted.get(2).get("numbers").get("id").asInt());
+        assertEquals(3, sorted.get(2).get("letters").get("id").asInt());
+        assertEquals(33333, sorted.get(2).get("numbers").get("value").asInt());
+        assertEquals("ccc", sorted.get(2).get("letters").get("value").asText());
+    }
+
+    @Test
+    void testOneToManyJoin() {
+
+        Deque<Map<String, JsonNode>> result = new ArrayDeque<>();
+
+        new Gingester().cli("" +
+                "-ss joinAs numbers -t JsonCreate [{id:1,value:11111},{id:2,value:22222}] -l JsonStream " +
+                "-ss joinAs letters -t JsonCreate [{id:1,value:'aaa'},{id:1,value:'AAA'},{id:2,value:'bbb'},{id:2,value:'BBB'}] -l JsonStream " +
+                "-t JsonStream '$[*]' " +
+                "-s value " +
+                "-t JsonPath '$.id' " +
+                "-t OuterJoin")
+                .attach(result::add)
+                .run();
+
+        List<Map<String, JsonNode>> sorted = result.stream()
+                .sorted(Comparator.comparing(i -> i.get("numbers").get("id").asText() + i.get("letters").get("value").asText()))
+                .collect(Collectors.toList());
+
+        assertEquals(1, sorted.get(0).get("numbers").get("id").asInt());
+        assertEquals(1, sorted.get(0).get("letters").get("id").asInt());
+        assertEquals(11111, sorted.get(0).get("numbers").get("value").asInt());
+        assertEquals("AAA", sorted.get(0).get("letters").get("value").asText());
+
+        assertEquals(1, sorted.get(1).get("numbers").get("id").asInt());
+        assertEquals(1, sorted.get(1).get("letters").get("id").asInt());
+        assertEquals(11111, sorted.get(1).get("numbers").get("value").asInt());
+        assertEquals("aaa", sorted.get(1).get("letters").get("value").asText());
+
+        assertEquals(2, sorted.get(2).get("numbers").get("id").asInt());
+        assertEquals(2, sorted.get(2).get("letters").get("id").asInt());
+        assertEquals(22222, sorted.get(2).get("numbers").get("value").asInt());
+        assertEquals("BBB", sorted.get(2).get("letters").get("value").asText());
+
+        assertEquals(2, sorted.get(3).get("numbers").get("id").asInt());
+        assertEquals(2, sorted.get(3).get("letters").get("id").asInt());
+        assertEquals(22222, sorted.get(3).get("numbers").get("value").asInt());
+        assertEquals("bbb", sorted.get(3).get("letters").get("value").asText());
+    }
+
+    @Test
+    void testJoin3() {
+
+        Deque<Map<String, JsonNode>> result = new ArrayDeque<>();
+
+        new Gingester().cli("" +
+                "-ss joinAs numbers -t JsonCreate [{id:1,value:11111},{id:2,value:22222},{id:3,value:33333}] -l JsonStream " +
+                "-ss joinAs upper -t JsonCreate [{id:1,value:'AAA'},{id:2,value:'BBB'},{id:3,value:'CCC'}] -l JsonStream " +
+                "-ss joinAs lower -t JsonCreate [{id:1,value:'aaa'},{id:2,value:'bbb'},{id:3,value:'ccc'}] -l JsonStream " +
+                "-t JsonStream '$[*]' " +
+                "-s value " +
+                "-t JsonPath '$.id' " +
+                "-t OuterJoin")
+                .attach(result::add)
+                .run();
+
+        List<Map<String, JsonNode>> sorted = result.stream()
+                .sorted(Comparator.comparing(i -> i.get("numbers").get("id").asText()))
+                .collect(Collectors.toList());
+
+        assertEquals(1, sorted.get(0).get("numbers").get("id").asInt());
+        assertEquals(1, sorted.get(0).get("upper").get("id").asInt());
+        assertEquals(1, sorted.get(0).get("lower").get("id").asInt());
+        assertEquals(11111, sorted.get(0).get("numbers").get("value").asInt());
+        assertEquals("AAA", sorted.get(0).get("upper").get("value").asText());
+        assertEquals("aaa", sorted.get(0).get("lower").get("value").asText());
+
+        assertEquals(2, sorted.get(1).get("numbers").get("id").asInt());
+        assertEquals(2, sorted.get(1).get("upper").get("id").asInt());
+        assertEquals(2, sorted.get(1).get("lower").get("id").asInt());
+        assertEquals(22222, sorted.get(1).get("numbers").get("value").asInt());
+        assertEquals("BBB", sorted.get(1).get("upper").get("value").asText());
+        assertEquals("bbb", sorted.get(1).get("lower").get("value").asText());
+
+        assertEquals(3, sorted.get(2).get("numbers").get("id").asInt());
+        assertEquals(3, sorted.get(2).get("upper").get("id").asInt());
+        assertEquals(3, sorted.get(2).get("lower").get("id").asInt());
+        assertEquals(33333, sorted.get(2).get("numbers").get("value").asInt());
+        assertEquals("CCC", sorted.get(2).get("upper").get("value").asText());
+        assertEquals("ccc", sorted.get(2).get("lower").get("value").asText());
+    }
+}
