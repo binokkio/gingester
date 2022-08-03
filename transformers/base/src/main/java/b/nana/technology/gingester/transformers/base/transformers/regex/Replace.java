512c45d31b7bf9a694350ae75c94c47e44a4cd96
Binokkio <binokkio@b.nana.technology>
2022-08-03 23:47:22 +0200

    Improve RegexReplace parameters parsing

--- transformers/base/src/main/java/b/nana/technology/gingester/transformers/base/transformers/regex/Replace.java
+++ transformers/base/src/main/java/b/nana/technology/gingester/transformers/base/transformers/regex/Replace.java
@@ -1,13 +1,14 @@
 package b.nana.technology.gingester.transformers.base.transformers.regex;
 
+import b.nana.technology.gingester.core.configuration.NormalizingDeserializer;
 import b.nana.technology.gingester.core.controller.Context;
 import b.nana.technology.gingester.core.receiver.Receiver;
 import b.nana.technology.gingester.core.template.Template;
 import b.nana.technology.gingester.core.template.TemplateMapper;
 import b.nana.technology.gingester.core.template.TemplateParameters;
 import b.nana.technology.gingester.core.transformer.Transformer;
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
 import java.util.regex.Pattern;
 
@@ -28,17 +29,17 @@ public final class Replace implements Transformer<String, String> {
         out.accept(context, pattern.matcher(in).replaceAll(replacement));
     }
 
+    @JsonDeserialize(using = Parameters.Deserializer.class)
     public static class Parameters {
+        public static class Deserializer extends NormalizingDeserializer<Parameters> {
+            public Deserializer() {
+                super(Parameters.class);
+                rule(JsonNode::isTextual, regex -> o("regex", regex));
+                rule(JsonNode::isArray, array -> o("regex", array.get(0), "replacement", array.get(1)));
+            }
+        }
 
         public TemplateParameters regex;
         public TemplateParameters replacement = new TemplateParameters("_", true);
-
-        @JsonCreator
-        public Parameters() {}
-
-        @JsonCreator
-        public Parameters(@JsonProperty("regex") TemplateParameters regex) {
-            this.regex = regex;
-        }
     }
 }
