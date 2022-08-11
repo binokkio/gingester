2c7c3ba8e177499d942ff314763f5548fae543a7
Binokkio <binokkio@b.nana.technology>
2022-08-11 07:48:55 +0200

    Reduce diff with develop

--- core/src/test/java/b/nana/technology/gingester/core/DiamondRouteTest.java
+++ core/src/test/java/b/nana/technology/gingester/core/DiamondRouteTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class DiamondRouteTest {
 
     @Test
-    void testFlowBuilder() {
+    void test() {
 
         ArrayDeque<String> results = new ArrayDeque<>();
 
