3c316f76e0e7566005d785960281ebfef1cec8db
Binokkio <binokkio@b.nana.technology>
2022-08-11 10:02:55 +0200

    Remove BaseConfiguration

--- core/src/main/java/b/nana/technology/gingester/core/configuration/SetupControls.java
+++ core/src/main/java/b/nana/technology/gingester/core/configuration/SetupControls.java
@@ -9,13 +9,17 @@ import java.lang.reflect.Method;
 import java.util.List;
 import java.util.Optional;
 
-public final class SetupControls extends BaseConfiguration<SetupControls> {
+public final class SetupControls {
 
+    private Integer maxWorkers;
+    private Integer maxQueueSize;
+    private Integer maxBatchSize;
+    private List<String> links;
+    private List<String> syncs;
+    private List<String> excepts;
     private boolean requireOutgoingSync;
     private boolean requireOutgoingAsync;
     private Counter acksCounter;
-
-
 
     public SetupControls(Transformer<?, ?> transformer) {
 
@@ -40,6 +44,36 @@ public final class SetupControls extends BaseConfiguration<SetupControls> {
 
 
 
+    public SetupControls maxWorkers(Integer maxWorkers) {
+        this.maxWorkers = maxWorkers;
+        return this;
+    }
+
+    public SetupControls maxQueueSize(Integer maxQueueSize) {
+        this.maxQueueSize = maxQueueSize;
+        return this;
+    }
+
+    public SetupControls maxBatchSize(Integer maxBatchSize) {
+        this.maxBatchSize = maxBatchSize;
+        return this;
+    }
+
+    public SetupControls links(List<String> links) {
+        this.links = links;
+        return this;
+    }
+
+    public SetupControls syncs(List<String> syncs) {
+        this.syncs = syncs;
+        return this;
+    }
+
+    public SetupControls excepts(List<String> excepts) {
+        this.excepts = excepts;
+        return this;
+    }
+
     public SetupControls requireOutgoingSync() {
         this.requireOutgoingSync = true;
         return this;
@@ -58,6 +92,30 @@ public final class SetupControls extends BaseConfiguration<SetupControls> {
     }
 
 
+
+    public Optional<Integer> getMaxWorkers() {
+        return Optional.ofNullable(maxWorkers);
+    }
+
+    public Optional<Integer> getMaxQueueSize() {
+        return Optional.ofNullable(maxQueueSize);
+    }
+
+    public Optional<Integer> getMaxBatchSize() {
+        return Optional.ofNullable(maxBatchSize);
+    }
+
+    public Optional<List<String>> getLinks() {
+        return Optional.ofNullable(links);
+    }
+
+    public Optional<List<String>> getSyncs() {
+        return Optional.ofNullable(syncs);
+    }
+
+    public Optional<List<String>> getExcepts() {
+        return Optional.ofNullable(excepts);
+    }
 
     public boolean getRequireOutgoingSync() {
         return requireOutgoingSync;
