a5e2744a98cf92c08a3368c7585c842d253f0d0b
Binokkio <binokkio@b.nana.technology>
2022-08-21 17:08:57 +0200

    Support templating in JsonWrap key parameter

---
+++ transformers/base/src/test/java/b/nana/technology/gingester/transformers/base/transformers/json/WrapTest.java
@@ -0,0 +1,27 @@
+package b.nana.technology.gingester.transformers.base.transformers.json;
+
+import b.nana.technology.gingester.core.FlowBuilder;
+import com.fasterxml.jackson.databind.JsonNode;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WrapTest {
+
+    @Test
+    void testKeyTemplate() {
+
+        AtomicReference<JsonNode> result = new AtomicReference<>();
+
+        new FlowBuilder().cli("" +
+                        "-ss key message " +
+                        "-t JsonDef '{\"hello\":\"world\"}' " +
+                        "-t JsonWrap '${key}'")
+                .add(result::set)
+                .run();
+
+        assertEquals("world", result.get().get("message").get("hello").asText());
+    }
+}
