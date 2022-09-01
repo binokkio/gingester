09d02e51bd141560ada5dc708c698422264a4945
Binokkio <binokkio@b.nana.technology>
2022-09-01 19:24:06 +0200

    Restore -h/--help flag

--- core/src/main/java/b/nana/technology/gingester/core/Main.java
+++ core/src/main/java/b/nana/technology/gingester/core/Main.java
@@ -4,6 +4,7 @@ import b.nana.technology.gingester.core.transformer.TransformerFactory;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
+import java.util.Arrays;
 
 import static java.util.Objects.requireNonNull;
 
@@ -12,7 +13,7 @@ public final class Main {
     private Main() {}
 
     public static void main(String[] args) {
-        if (args.length == 0) {
+        if (args.length == 0 || Arrays.stream(args).anyMatch(s -> s.equals("-h") || s.equals("--help"))) {
             printHelp();
         } else {
             new FlowBuilder()
