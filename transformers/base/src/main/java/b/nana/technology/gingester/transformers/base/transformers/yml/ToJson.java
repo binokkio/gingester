0e86ec4d696b5c58390caa6ee15184c69f3d9de0
Binokkio <binokkio@b.nana.technology>
2022-10-21 08:11:31 +0200

    Add YmlToJson transformer

---
+++ transformers/base/src/main/java/b/nana/technology/gingester/transformers/base/transformers/yml/ToJson.java
@@ -0,0 +1,25 @@
+package b.nana.technology.gingester.transformers.base.transformers.yml;
+
+import b.nana.technology.gingester.core.controller.Context;
+import b.nana.technology.gingester.core.receiver.Receiver;
+import b.nana.technology.gingester.core.transformer.Transformer;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public final class ToJson implements Transformer<InputStream, JsonNode> {
+
+    private final ObjectMapper objectMapper;
+
+    public ToJson() {
+        objectMapper = new ObjectMapper(new YAMLFactory());
+    }
+
+    @Override
+    public void transform(Context context, InputStream in, Receiver<JsonNode> out) throws IOException {
+        out.accept(context, objectMapper.readTree(in));
+    }
+}
