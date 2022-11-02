74fd83dee618d84ba7ca496130f5b34653b5e409
Binokkio <binokkio@b.nana.technology>
2022-11-02 21:15:26 +0100

    Support named groups in RegexGroup

--- transformers/base/src/main/java/b/nana/technology/gingester/transformers/base/transformers/regex/Group.java
+++ transformers/base/src/main/java/b/nana/technology/gingester/transformers/base/transformers/regex/Group.java
@@ -1,35 +1,48 @@
 package b.nana.technology.gingester.transformers.base.transformers.regex;
 
+import b.nana.technology.gingester.core.configuration.NormalizingDeserializer;
 import b.nana.technology.gingester.core.controller.Context;
 import b.nana.technology.gingester.core.receiver.Receiver;
 import b.nana.technology.gingester.core.transformer.Transformer;
-import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
 import java.util.regex.Matcher;
 
 public final class Group implements Transformer<Matcher, String> {
 
     private final int group;
+    private final String groupName;
 
     public Group(Parameters parameters) {
-        group = parameters.group;
+
+        if (parameters.group != null && parameters.groupName != null)
+            throw new IllegalArgumentException("Both group and groupName given");
+
+        group = parameters.group != null ? parameters.group : -1;
+        groupName = parameters.groupName;
     }
 
     @Override
     public void transform(Context context, Matcher in, Receiver<String> out) {
-        out.accept(context, in.group(group));
+        if (groupName != null) {
+            out.accept(context, in.group(groupName));
+        } else {
+            out.accept(context, in.group(group));
+        }
     }
 
+    @JsonDeserialize(using = Parameters.Deserializer.class)
     public static class Parameters {
-
-        public int group;
-
-        @JsonCreator
-        public Parameters() {}
-
-        @JsonCreator
-        public Parameters(int group) {
-            this.group = group;
+        public static class Deserializer extends NormalizingDeserializer<Parameters> {
+            public Deserializer() {
+                super(Parameters.class);
+                rule(JsonNode::isInt, i -> o("group", i));
+                rule(JsonNode::isTextual, i -> o("groupName", i));
+            }
         }
+
+        public Integer group;
+        public String groupName;
     }
 }
