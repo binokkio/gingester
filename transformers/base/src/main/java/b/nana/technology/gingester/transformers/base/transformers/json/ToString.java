dd76a69e6a5dd96e396443d305e814c31dbf98dd
Binokkio <binokkio@b.nana.technology>
2022-11-02 20:47:55 +0100

    Drop quotes from JsonToString output for TextNodes

--- transformers/base/src/main/java/b/nana/technology/gingester/transformers/base/transformers/json/ToString.java
+++ transformers/base/src/main/java/b/nana/technology/gingester/transformers/base/transformers/json/ToString.java
@@ -1,15 +1,16 @@
 package b.nana.technology.gingester.transformers.base.transformers.json;
 
 import b.nana.technology.gingester.core.annotations.Pure;
+import b.nana.technology.gingester.core.configuration.NormalizingDeserializer;
 import b.nana.technology.gingester.core.controller.Context;
 import b.nana.technology.gingester.core.receiver.Receiver;
 import b.nana.technology.gingester.core.transformer.Transformer;
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.core.util.DefaultIndenter;
 import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
 @Pure
 public final class ToString implements Transformer<JsonNode, String> {
@@ -25,24 +26,26 @@ public final class ToString implements Transformer<JsonNode, String> {
 
     @Override
     public void transform(Context context, JsonNode in, Receiver<String> out) throws Exception {
-        if (pretty) {
+        if (in.isTextual()) {
+            out.accept(context, in.textValue());
+        } else if (pretty) {
             out.accept(context, objectWriter.writeValueAsString(in));
         } else {
             out.accept(context, in.toString());
         }
     }
 
+    @JsonDeserialize(using = Parameters.Deserializer.class)
     public static class Parameters {
+        public static class Deserializer extends NormalizingDeserializer<Parameters> {
+            public Deserializer() {
+                super(Parameters.class);
+                rule(JsonNode::isBoolean, bool -> o("pretty", bool.booleanValue()));
+                rule(JsonNode::isTextual, text -> o("pretty", text.textValue().equals("pretty")));
+            }
+        }
 
         public boolean pretty;
-
-        @JsonCreator
-        public Parameters() {}
-
-        @JsonCreator
-        public Parameters(boolean pretty) {
-            this.pretty = pretty;
-        }
     }
 
     private static class Printer extends DefaultPrettyPrinter {
