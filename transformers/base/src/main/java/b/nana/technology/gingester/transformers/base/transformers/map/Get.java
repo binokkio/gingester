d9c9cacd59516962cf4968b0936cd9610bc07681
Binokkio <binokkio@b.nana.technology>
2022-11-07 22:01:09 +0100

    Improve MapGet

--- transformers/base/src/main/java/b/nana/technology/gingester/transformers/base/transformers/map/Get.java
+++ transformers/base/src/main/java/b/nana/technology/gingester/transformers/base/transformers/map/Get.java
@@ -1,10 +1,12 @@
 package b.nana.technology.gingester.transformers.base.transformers.map;
 
+import b.nana.technology.gingester.core.configuration.NormalizingDeserializer;
 import b.nana.technology.gingester.core.controller.Context;
 import b.nana.technology.gingester.core.controller.FetchKey;
 import b.nana.technology.gingester.core.receiver.Receiver;
 import b.nana.technology.gingester.core.transformer.Transformer;
-import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
 import java.util.Map;
 import java.util.NoSuchElementException;
@@ -12,11 +14,11 @@ import java.util.NoSuchElementException;
 public final class Get implements Transformer<Object, Object> {
 
     private final FetchKey fetchMap;
-    private final boolean throwOnEmptyGet;
+    private final boolean optional;
 
     public Get(Parameters parameters) {
         fetchMap = parameters.map;
-        throwOnEmptyGet = parameters.throwOnEmptyGet;
+        optional = parameters.optional;
     }
 
     @Override
@@ -25,24 +27,24 @@ public final class Get implements Transformer<Object, Object> {
         Map<?, ?> map = (Map<?, ?>) context.require(fetchMap);
         Object result = map.get(in);
 
-        if (throwOnEmptyGet && result == null) {
+        if (result != null) {
+            out.accept(context, result);
+        } else if (!optional) {
             throw new NoSuchElementException(fetchMap + " :: " + in);
         }
-
-        out.accept(context, result);
     }
 
+    @JsonDeserialize(using = Parameters.Deserializer.class)
     public static class Parameters {
+        public static class Deserializer extends NormalizingDeserializer<Parameters> {
+            public Deserializer() {
+                super(Parameters.class);
+                rule(JsonNode::isTextual, map -> o("map", map));
+                rule(JsonNode::isArray, array -> o("map", array.get(0), "optional", f("optional", array.get(1))));
+            }
+        }
 
         public FetchKey map = new FetchKey(1);
-        public boolean throwOnEmptyGet = false;
-
-        @JsonCreator
-        public Parameters() {}
-
-        @JsonCreator
-        public Parameters(FetchKey map) {
-            this.map = map;
-        }
+        public boolean optional = false;
     }
 }
