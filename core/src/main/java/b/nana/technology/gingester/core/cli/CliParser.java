7a587c1e610ac0724a044cafa86a5880c2ce63ba
Binokkio <binokkio@b.nana.technology>
2022-11-12 14:59:33 +0100

    Extract block comment removal code to dedicated class

--- core/src/main/java/b/nana/technology/gingester/core/cli/CliParser.java
+++ core/src/main/java/b/nana/technology/gingester/core/cli/CliParser.java
@@ -22,9 +22,9 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.stream.Stream;
+
+import static b.nana.technology.gingester.core.cli.BlockCommentRemover.removeBlockComments;
 
 public final class CliParser {
 
@@ -37,9 +37,6 @@ public final class CliParser {
             .setSerializationInclusion(JsonInclude.Include.NON_NULL);
 
     static final ObjectReader OBJECT_READER = OBJECT_MAPPER.reader();
-
-    private static final Pattern BLOCK_COMMENT = Pattern.compile("\\s(\\+{2,})\\s");
-    private static final Pattern BLOCK_COMMENT_ARG = Pattern.compile("\\+{2,}");
 
     private CliParser() {}
 
@@ -56,63 +53,6 @@ public final class CliParser {
             parse(target, CliSplitter.split(cli));
         } catch (IOException e) {
             throw new RuntimeException(e);
-        }
-    }
-
-    private static String removeBlockComments(String source) {
-        int level = 0;
-        int begin = -1;
-        int end = -1;
-        Matcher matcher = BLOCK_COMMENT.matcher(source);
-        while (matcher.find()) {
-            int foundLevel = matcher.group(1).length();
-            if (foundLevel > level) {
-                level = foundLevel;
-                begin = matcher.start(1);
-                end = -1;
-            } else if (foundLevel == level && end == -1) {
-                end = matcher.start(1) + foundLevel;
-            }
-        }
-        if (level > 0) {
-            String result = source.substring(0, begin);
-            if (end != -1) result += source.substring(end);
-            return removeBlockComments(result);
-        } else {
-            return source;
-        }
-    }
-
-    private static String[] removeBlockComments(String[] args) {
-        int level = 0;
-        int begin = -1;
-        int end = -1;
-        for (int i = 0; i < args.length; i++) {
-            Matcher matcher = BLOCK_COMMENT_ARG.matcher(args[i]);
-            if (matcher.matches()) {
-                int foundLevel = matcher.group().length();
-                if (foundLevel > level) {
-                    level = foundLevel;
-                    begin = i;
-                    end = -1;
-                } else if (foundLevel == level && end == -1) {
-                    end = i;
-                }
-            }
-        }
-        if (level > 0) {
-            String[] result;
-            if (end == -1) {
-                result = new String[begin];
-                System.arraycopy(args, 0, result, 0, begin);
-            } else {
-                result = new String[begin + (args.length - end - 1)];
-                System.arraycopy(args, 0, result, 0, begin);
-                System.arraycopy(args, end + 1, result, begin, args.length - end - 1);
-            }
-            return removeBlockComments(result);
-        } else {
-            return args;
         }
     }
 
