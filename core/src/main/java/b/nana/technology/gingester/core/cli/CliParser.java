94fe03508920a39c329830668257a647b7b79aad
Binokkio <binokkio@b.nana.technology>
2022-11-12 12:16:52 +0100

    CLI block comment preprocessing PoC

--- core/src/main/java/b/nana/technology/gingester/core/cli/CliParser.java
+++ core/src/main/java/b/nana/technology/gingester/core/cli/CliParser.java
@@ -22,6 +22,8 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 public final class CliParser {
@@ -36,17 +38,20 @@ public final class CliParser {
 
     static final ObjectReader OBJECT_READER = OBJECT_MAPPER.reader();
 
+    private static final Pattern BLOCK_COMMENT = Pattern.compile("\\s(\\+{2,})\\s");
+    private static final Pattern BLOCK_COMMENT_ARG = Pattern.compile("\\+{2,}");
+
     private CliParser() {}
 
     public static void parse(FlowBuilder target, String template, Object parameters) {
-        String cli = FreemarkerTemplateFactory.createCliTemplate("string", template).render(parameters);
+        String cli = FreemarkerTemplateFactory.createCliTemplate("string", removeBlockComments(template)).render(parameters);
         parse(target, CliSplitter.split(cli));
     }
 
     public static void parse(FlowBuilder target, URL template, Object parameters) {
         try (InputStream templateStream = template.openStream()) {
             String templateName = template.toString();
-            String templateSource = new String(templateStream.readAllBytes(), StandardCharsets.UTF_8);
+            String templateSource = removeBlockComments(new String(templateStream.readAllBytes(), StandardCharsets.UTF_8));
             String cli = FreemarkerTemplateFactory.createCliTemplate(templateName, templateSource).render(parameters);
             parse(target, CliSplitter.split(cli));
         } catch (IOException e) {
@@ -54,7 +59,66 @@ public final class CliParser {
         }
     }
 
+    private static String removeBlockComments(String source) {
+        int level = 0;
+        int begin = -1;
+        int end = -1;
+        Matcher matcher = BLOCK_COMMENT.matcher(source);
+        while (matcher.find()) {
+            int foundLevel = matcher.group(1).length();
+            if (foundLevel > level) {
+                level = foundLevel;
+                begin = matcher.start(1);
+                end = -1;
+            } else if (foundLevel == level && end == -1) {
+                end = matcher.start(1) + foundLevel;
+            }
+        }
+        if (level > 0) {
+            String result = source.substring(0, begin);
+            if (end != -1) result += source.substring(end);
+            return removeBlockComments(result);
+        } else {
+            return source;
+        }
+    }
+
+    private static String[] removeBlockComments(String[] args) {
+        int level = 0;
+        int begin = -1;
+        int end = -1;
+        for (int i = 0; i < args.length; i++) {
+            Matcher matcher = BLOCK_COMMENT_ARG.matcher(args[i]);
+            if (matcher.matches()) {
+                int foundLevel = matcher.group().length();
+                if (foundLevel > level) {
+                    level = foundLevel;
+                    begin = i;
+                    end = -1;
+                } else if (foundLevel == level && end == -1) {
+                    end = i;
+                }
+            }
+        }
+        if (level > 0) {
+            String[] result;
+            if (end == -1) {
+                result = new String[begin];
+                System.arraycopy(args, 0, result, 0, begin);
+            } else {
+                result = new String[begin + (args.length - end - 1)];
+                System.arraycopy(args, 0, result, 0, begin);
+                System.arraycopy(args, end + 1, result, begin, args.length - end - 1);
+            }
+            return removeBlockComments(result);
+        } else {
+            return args;
+        }
+    }
+
     public static void parse(FlowBuilder target, String[] args) {
+
+        args = removeBlockComments(args);
 
         for (int i = 0; i < args.length; i++) {
 
@@ -66,12 +130,6 @@ public final class CliParser {
 
                 case "+":
                     i++;
-                    break;
-
-                case "++":
-                    // ignore all args until the next occurrence of "++"
-                    // noinspection StatementWithEmptyBody
-                    while (++i < args.length && !args[i].equals("++"));
                     break;
 
                 case "-r":
