c16262523c607c751b40fada8897ceb8364d7e84
Binokkio <binokkio@b.nana.technology>
2022-12-20 21:22:34 +0100

    Simplify SmtpServer

--- transformers/smtp/src/main/java/b/nana/technology/gingester/transformers/smtp/Server.java
+++ transformers/smtp/src/main/java/b/nana/technology/gingester/transformers/smtp/Server.java
@@ -6,21 +6,11 @@ import b.nana.technology.gingester.core.receiver.Receiver;
 import b.nana.technology.gingester.core.transformer.Transformer;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import jakarta.mail.Header;
-import jakarta.mail.MessagingException;
-import jakarta.mail.Session;
-import jakarta.mail.internet.MimeMessage;
 import org.subethamail.smtp.server.SMTPServer;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Enumeration;
-import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.Properties;
 
-public final class Server implements Transformer<Object, InputStream> {
+public final class Server implements Transformer<Object, byte[]> {
 
     private final int port;
 
@@ -29,38 +19,19 @@ public final class Server implements Transformer<Object, InputStream> {
     }
 
     @Override
-    public void transform(Context context, Object in, Receiver<InputStream> out) {
+    public void transform(Context context, Object in, Receiver<byte[]> out) {
 
         SMTPServer server = SMTPServer
                 .port(port)
-                .messageHandler(
-                        (messageContext, from, to, data) -> {
-
-                            try {
-                                MimeMessage mimeMessage = new MimeMessage(
-                                        Session.getDefaultInstance(new Properties()),
-                                        new ByteArrayInputStream(data)
-                                );
-
-                                Map<String, String> headers = new LinkedHashMap<>();
-                                Enumeration<Header> headersEnumeration = mimeMessage.getAllHeaders();
-                                while (headersEnumeration.hasMoreElements()) {
-                                    Header header = headersEnumeration.nextElement();
-                                    headers.put(header.getName(), header.getValue());
-                                }
-
-                                out.accept(
-                                        context.stash(Map.of(
-                                                "headers", headers
-                                        )),
-                                        mimeMessage.getInputStream()
-                                );
-
-                            } catch (IOException | MessagingException e) {
-                                throw new RuntimeException(e);
-                            }
-
-                        })
+                .messageHandler((messageContext, from, to, data) ->
+                        out.accept(
+                                context.stash(Map.of(
+                                        "from", from,
+                                        "to", to
+                                )),
+                                data
+                        )
+                )
                 .build();
 
         server.start();
