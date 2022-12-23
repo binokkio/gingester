88c900d36da8cf2aeb297666f1c6cf1f49e786f3
Binokkio <binokkio@b.nana.technology>
2022-12-23 21:06:34 +0100

    Improve Exec transformer, introduce FlagOrderDeserializer

--- transformers/base/src/main/java/b/nana/technology/gingester/transformers/base/transformers/exec/Exec.java
+++ transformers/base/src/main/java/b/nana/technology/gingester/transformers/base/transformers/exec/Exec.java
@@ -2,7 +2,8 @@ package b.nana.technology.gingester.transformers.base.transformers.exec;
 
 import b.nana.technology.gingester.core.annotations.Names;
 import b.nana.technology.gingester.core.cli.CliSplitter;
-import b.nana.technology.gingester.core.configuration.NormalizingDeserializer;
+import b.nana.technology.gingester.core.configuration.FlagOrderDeserializer;
+import b.nana.technology.gingester.core.configuration.Order;
 import b.nana.technology.gingester.core.configuration.SetupControls;
 import b.nana.technology.gingester.core.controller.Context;
 import b.nana.technology.gingester.core.receiver.Receiver;
@@ -10,85 +11,146 @@ import b.nana.technology.gingester.core.template.Template;
 import b.nana.technology.gingester.core.template.TemplateMapper;
 import b.nana.technology.gingester.core.template.TemplateParameters;
 import b.nana.technology.gingester.core.transformer.Transformer;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @Names(1)
-public final class Exec implements Transformer<InputStream, InputStream> {
+public final class Exec implements Transformer<Object, Object> {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Exec.class);
 
-    private final ExecutorService errDrainer = Executors.newCachedThreadPool();
+    private final ExecutorService drainers = Executors.newCachedThreadPool();
 
     private final Template commandTemplate;
     private final TemplateMapper<File> workDirTemplate;
+    private final Parameters.StreamHandling stdout;
+    private final Parameters.StreamHandling stderr;
+    private final boolean stdin;
+    private final boolean logPid;
 
     public Exec(Parameters parameters) {
         commandTemplate = Context.newTemplate(parameters.command);
         workDirTemplate = Context.newTemplateMapper(parameters.workDir, s -> new File(s).getAbsoluteFile());
+        stdout = parameters.stdout;
+        stderr = parameters.stderr;
+        stdin = parameters.stdin;
+        logPid = parameters.logPid;
+    }
+
+    private boolean yieldsStream() {
+        return stdout == Parameters.StreamHandling.YIELD || stderr == Parameters.StreamHandling.YIELD;
     }
 
     @Override
     public void setup(SetupControls controls) {
-        controls.requireOutgoingAsync();
+        if (yieldsStream())
+            controls.requireOutgoingAsync();
     }
 
     @Override
-    public void transform(Context context, InputStream in, Receiver<InputStream> out) throws Exception {
+    public Class<?> getInputType() {
+        return stdin ? InputStream.class : Object.class;
+    }
+
+    @Override
+    public Object getOutputType() {
+        return yieldsStream() ?
+                InputStream.class :
+                Object.class;
+    }
+
+    @Override
+    public void transform(Context context, Object in, Receiver<Object> out) throws Exception {
 
         String command = commandTemplate.render(context, in);
         String[] args = CliSplitter.split(command);
-
         Runtime runtime = Runtime.getRuntime();
         Process process = runtime.exec(args, null, workDirTemplate.render(context, in));
+        long pid = process.pid();
 
-        out.accept(context.stash("description", command), process.getInputStream());
+        if (logPid)
+            LOGGER.info("Process {} started: {}", pid, command);
 
-        // TODO emit stderr instead, to a transformer specified by a parameter
-        BufferedReader processStdErr = new BufferedReader(new InputStreamReader(process.getErrorStream(), StandardCharsets.UTF_8));
-        errDrainer.submit(() -> {
-            while (true) {
-                try {
-                    String line = processStdErr.readLine();
-                    if (line == null) break;
-                    LOGGER.info("{}: {}", command, line);
-                } catch (IOException e) {
-                    throw new RuntimeException("Reading " + command + " stderr threw", e);
-                }
-            }
-        });
+        handleStream(pid, command, "stdout", stdout, process.getInputStream(), context, out);
+        handleStream(pid, command, "stderr", stderr, process.getErrorStream(), context, out);
 
         OutputStream processStdIn = process.getOutputStream();
-        in.transferTo(processStdIn);
+        if (stdin) ((InputStream) in).transferTo(processStdIn);
         processStdIn.close();
 
         int resultCode = process.waitFor();
         // TODO throw an exception on non-zero resultCode?
+
+        if (!yieldsStream())
+            out.accept(context, "exec finish signal");
+    }
+
+    private void handleStream(long pid, String command, String streamName, Parameters.StreamHandling handling, InputStream stream, Context context, Receiver<Object> out) {
+        switch (handling) {
+
+            case YIELD:
+                out.accept(context.stash(Map.of(
+                        "description", command + " (" + streamName + ")",
+                        "command", command,
+                        "stream", streamName
+                )), stream);
+                break;
+
+            case LOG:
+                drainers.submit(() -> {
+                    try {
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8));
+                        String prefix = pid + " " + streamName + ": ";
+                        String line;
+                        while ((line = reader.readLine()) != null) {
+                            LOGGER.info("{}: {}", prefix, line);
+                        }
+                    } catch (IOException e) {
+                        // TODO
+                        throw new RuntimeException("Reading " + command + " stderr threw", e);
+                    }
+                });
+                break;
+
+            case IGNORE:
+                drainers.submit(() -> {
+                    try {
+                        while (stream.skip(Long.MAX_VALUE) > 0) ;
+                    } catch (IOException e) {
+                        // TODO
+                    }
+                });
+                break;
+        }
     }
 
     @Override
     public void close() {
-        errDrainer.shutdown();
+        drainers.shutdown();
     }
 
-    @JsonDeserialize(using = Parameters.Deserializer.class)
+    @JsonDeserialize(using = FlagOrderDeserializer.class)
+    @Order({"command", "workDir", "stdout", "stderr"})
     public static class Parameters {
-        public static class Deserializer extends NormalizingDeserializer<Parameters> {
-            public Deserializer() {
-                super(Parameters.class);
-                rule(JsonNode::isTextual, command -> o("command", command));
-                rule(JsonNode::isObject, o -> o.has("template") ? o("command", o) : o);
-            }
-        }
 
         public TemplateParameters command;
         public TemplateParameters workDir = new TemplateParameters("", true);
+        public StreamHandling stdout = StreamHandling.YIELD;
+        public StreamHandling stderr = StreamHandling.LOG;
+        public boolean stdin = true;
+        public boolean logPid = false;
+
+        public enum StreamHandling {
+            YIELD,
+            LOG,
+            IGNORE
+        }
     }
 }
--- transformers/base/src/test/java/b/nana/technology/gingester/transformers/base/transformers/exec/ExecTest.java
+++ transformers/base/src/test/java/b/nana/technology/gingester/transformers/base/transformers/exec/ExecTest.java
@@ -40,7 +40,22 @@ class ExecTest {
     }
 
     @Test
-    void testCommandStringWithWorkDir() {
+    void testCommandStringWithWorkDirArray() {
+
+        AtomicReference<String> result = new AtomicReference<>();
+
+        new FlowBuilder().cli("" +
+                "-t StringDef 'Hello, World!' " +
+                "-t Exec '/bin/cat' '/' " +
+                "-t InputStreamToString")
+                .add(result::set)
+                .run();
+
+        assertEquals("Hello, World!", result.get());
+    }
+
+    @Test
+    void testCommandStringWithWorkDirObject() {
 
         AtomicReference<String> result = new AtomicReference<>();
 
@@ -55,7 +70,22 @@ class ExecTest {
     }
 
     @Test
-    void testCommandTemplateWithWorkDir() {
+    void testCommandTemplateWithWorkDirArray() {
+
+        AtomicReference<String> result = new AtomicReference<>();
+
+        new FlowBuilder().cli("" +
+                "-t StringDef 'Hello, World!' " +
+                "-t Exec '{template: \"/bin/cat\", is: \"STRING\"}' '/' " +
+                "-t InputStreamToString")
+                .add(result::set)
+                .run();
+
+        assertEquals("Hello, World!", result.get());
+    }
+
+    @Test
+    void testCommandTemplateWithWorkDirObject() {
 
         AtomicReference<String> result = new AtomicReference<>();
 
@@ -67,5 +97,33 @@ class ExecTest {
                 .run();
 
         assertEquals("Hello, World!", result.get());
+    }
+
+    @Test
+    void testNoStdIn() {
+
+        AtomicReference<String> result = new AtomicReference<>();
+
+        new FlowBuilder().cli("" +
+                "-t StringDef 'Hello, World!' " +
+                "-t Exec !stdin /bin/cat " +
+                "-t InputStreamToString")
+                .add(result::set)
+                .run();
+
+        assertEquals("", result.get());
+    }
+
+    @Test
+    void testIgnoreStdOut() {
+
+        AtomicReference<String> result = new AtomicReference<>();
+
+        new FlowBuilder().cli("" +
+                "-t Exec !stdin '/bin/dd bs=1024 count=1024 if=/dev/zero' . IGNORE IGNORE")
+                .add(result::set)
+                .run();
+
+        assertEquals("exec finish signal", result.get());
     }
 }
