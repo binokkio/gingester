a4bb59358e762fc40fac8d6c3f674b66a37c8d1a
Binokkio <binokkio@b.nana.technology>
2023-02-07 22:18:02 +0100

    Add XmlFromInputStream

--- transformers/base/src/main/java/b/nana/technology/gingester/transformers/base/BaseProvider.java
+++ transformers/base/src/main/java/b/nana/technology/gingester/transformers/base/BaseProvider.java
@@ -158,6 +158,7 @@ public final class BaseProvider implements Provider {
                 b.nana.technology.gingester.transformers.base.transformers.util.Latch.class,
                 b.nana.technology.gingester.transformers.base.transformers.util.Sample.class,
                 b.nana.technology.gingester.transformers.base.transformers.util.Throttle.class,
+                b.nana.technology.gingester.transformers.base.transformers.xml.FromInputStream.class,
                 b.nana.technology.gingester.transformers.base.transformers.xml.ToJson.class,
                 b.nana.technology.gingester.transformers.base.transformers.yaml.JsonToYaml.class,
                 b.nana.technology.gingester.transformers.base.transformers.yaml.ToJson.class
---
+++ transformers/base/src/main/java/b/nana/technology/gingester/transformers/base/transformers/xml/FromInputStream.java
@@ -0,0 +1,33 @@
+package b.nana.technology.gingester.transformers.base.transformers.xml;
+
+import b.nana.technology.gingester.core.annotations.Pure;
+import b.nana.technology.gingester.core.controller.Context;
+import b.nana.technology.gingester.core.receiver.Receiver;
+import b.nana.technology.gingester.core.transformer.Transformer;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Pure
+public final class FromInputStream implements Transformer<InputStream, Document> {
+
+    private final DocumentBuilder documentBuilder;
+
+    public FromInputStream() {
+        try {
+            documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        } catch (ParserConfigurationException e) {
+            throw new RuntimeException();
+        }
+    }
+
+    @Override
+    public void transform(Context context, InputStream in, Receiver<Document> out) throws IOException, SAXException {
+        out.accept(context, documentBuilder.parse(in));
+    }
+}
