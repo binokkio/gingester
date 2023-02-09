f7bc90684f37e0a46f5f9ae23fcc4ca75e366cab
Binokkio <binokkio@b.nana.technology>
2023-02-09 19:23:48 +0100

    Add statusCode and headers parameters to HttpRespond

--- transformers/jetty/src/main/java/b/nana/technology/gingester/transformers/jetty/http/Respond.java
+++ transformers/jetty/src/main/java/b/nana/technology/gingester/transformers/jetty/http/Respond.java
@@ -1,21 +1,35 @@
 package b.nana.technology.gingester.transformers.jetty.http;
 
+import b.nana.technology.gingester.core.configuration.NormalizingDeserializer;
 import b.nana.technology.gingester.core.controller.Context;
 import b.nana.technology.gingester.core.controller.FetchKey;
 import b.nana.technology.gingester.core.receiver.Receiver;
 import b.nana.technology.gingester.core.template.Template;
 import b.nana.technology.gingester.core.template.TemplateParameters;
 import b.nana.technology.gingester.core.transformer.Transformer;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import java.io.InputStream;
 import java.net.URLConnection;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public final class Respond implements Transformer<InputStream, String> {
 
+    private final int statusCode;
+    private final Map<String, Template> headers;
     private final FetchKey fetchHttpResponse = new FetchKey("http.response");
     private final Template contentTypeDetectionInputTemplate;
 
     public Respond(Parameters parameters) {
+        statusCode = parameters.statusCode;
+        headers = parameters.headers.entrySet().stream().collect(Collectors.toMap(
+                Map.Entry::getKey,
+                e -> Context.newTemplate(e.getValue())
+        ));
         contentTypeDetectionInputTemplate = Context.newTemplate(parameters.detectContentTypeFrom);
     }
 
@@ -24,6 +38,12 @@ public final class Respond implements Transformer<InputStream, String> {
 
         HttpResponse response = (HttpResponse) context.fetch(fetchHttpResponse)
                 .orElseThrow(() -> new IllegalStateException("Context did not come from HttpServer"));
+
+        if (statusCode != 0)
+            response.setStatus(statusCode);
+
+        headers.forEach((name, template) ->
+                response.addHeader(name, template.render(context, in)));
 
         if (!response.hasHeader("Content-Type")) {
 
@@ -41,7 +61,28 @@ public final class Respond implements Transformer<InputStream, String> {
         out.accept(context, "http respond signal");
     }
 
+    @JsonDeserialize(using = Parameters.Deserializer.class)
     public static class Parameters {
+        public static class Deserializer extends NormalizingDeserializer<Parameters> {
+            public Deserializer() {
+                super(Parameters.class);
+                rule(JsonNode::isInt, NormalizingDeserializer::a);
+                rule(JsonNode::isObject, o -> o.has("statusCode") || o.has("headers") || o.has("detectContentTypeFrom") ? o : a(o));
+                rule(JsonNode::isArray, a -> {
+                    ObjectNode result = JsonNodeFactory.instance.objectNode();
+                    for (int i = 0; i < a.size(); i++) {
+                        JsonNode entry = a.get(i);
+                        if (entry.isInt()) result.set("statusCode", entry);
+                        else if (entry.isObject()) result.set("headers", entry);
+                        else throw new IllegalArgumentException("Unexpected argument: " + entry);
+                    }
+                    return result;
+                });
+            }
+        }
+
+        public int statusCode;
+        public Map<String, TemplateParameters> headers = Map.of();
         public TemplateParameters detectContentTypeFrom = new TemplateParameters("${description!''}", false);
     }
 }
