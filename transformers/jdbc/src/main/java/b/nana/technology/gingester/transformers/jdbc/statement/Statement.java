165ae4a358e7ead62f9d07573c1fa2a1dd2554e4
Binokkio <binokkio@b.nana.technology>
2023-03-11 19:58:03 +0100

    Improve sql statement templating

--- transformers/jdbc/src/main/java/b/nana/technology/gingester/transformers/jdbc/statement/Statement.java
+++ transformers/jdbc/src/main/java/b/nana/technology/gingester/transformers/jdbc/statement/Statement.java
@@ -8,13 +8,31 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public abstract class Statement {
+
+    private static final Pattern PARAMETER = Pattern.compile(":([0-9a-zA-Z.]+)");
 
     private final PreparedStatement preparedStatement;
     private final List<Parameter> parameters;
 
     public Statement(Connection connection, String statement, List<JdbcTransformer.Parameters.Statement.Parameter> parameters, boolean returnGeneratedKeys) throws SQLException {
+
+        if (parameters.isEmpty()) {
+            parameters = new ArrayList<>();
+            Matcher matcher = PARAMETER.matcher(statement);
+            while (matcher.find()) {
+                JdbcTransformer.Parameters.Statement.Parameter p = new JdbcTransformer.Parameters.Statement.Parameter();
+                p.stash = matcher.group(1);
+                parameters.add(p);
+            }
+            statement = matcher.replaceAll("?");
+        } else {
+            // TODO log deprecation warning
+        }
+
         preparedStatement = returnGeneratedKeys ?
                 connection.prepareStatement(statement, java.sql.Statement.RETURN_GENERATED_KEYS) :
                 connection.prepareStatement(statement);
