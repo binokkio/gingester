17423dda43017381439bbdc5b209fba108bbb13c
Binokkio <binokkio@b.nana.technology>
2023-05-12 21:21:52 +0200

    Update example

--- transformers/base/src/main/java/b/nana/technology/gingester/transformers/base/transformers/string/Lowercase.java
+++ transformers/base/src/main/java/b/nana/technology/gingester/transformers/base/transformers/string/Lowercase.java
@@ -1,11 +1,13 @@
 package b.nana.technology.gingester.transformers.base.transformers.string;
 
+import b.nana.technology.gingester.core.annotations.Names;
 import b.nana.technology.gingester.core.controller.Context;
 import b.nana.technology.gingester.core.receiver.Receiver;
 import b.nana.technology.gingester.core.transformer.Transformer;
 
 import java.util.Locale;
 
+@Names(1)
 public final class Lowercase implements Transformer<String, String> {
 
     @Override
