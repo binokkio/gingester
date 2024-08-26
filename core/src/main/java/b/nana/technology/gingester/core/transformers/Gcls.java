49a76d7ebbfcd971ef41e30e06993284c385dc15
Binokkio <binokkio@b.nana.technology>
2024-08-26 22:02:22 +0200

    Include JSON Schema in Gcls output

--- core/src/main/java/b/nana/technology/gingester/core/transformers/Gcls.java
+++ core/src/main/java/b/nana/technology/gingester/core/transformers/Gcls.java
@@ -5,12 +5,29 @@ import b.nana.technology.gingester.core.controller.Context;
 import b.nana.technology.gingester.core.receiver.Receiver;
 import b.nana.technology.gingester.core.transformer.Transformer;
 import b.nana.technology.gingester.core.transformer.TransformerFactory;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.github.victools.jsonschema.generator.*;
 
 import java.util.List;
 import java.util.Optional;
 
 @Names(1)
 public final class Gcls implements Transformer<Object, Object> {
+
+    private final SchemaGenerator schemaGenerator;
+
+    public Gcls() {
+
+        SchemaGeneratorConfigBuilder configBuilder = new SchemaGeneratorConfigBuilder(SchemaVersion.DRAFT_2020_12, OptionPreset.PLAIN_JSON);
+
+        configBuilder
+                .without(Option.SCHEMA_VERSION_INDICATOR)
+                .with(Option.INLINE_ALL_SCHEMAS);
+
+        configBuilder.forTypesInGeneral().withPropertySorter((a, b) -> 0);
+
+        schemaGenerator = new SchemaGenerator(configBuilder.build());
+    }
 
     @Override
     public void transform(Context context, Object in, Receiver<Object> out) throws Exception {
@@ -25,12 +42,13 @@ public final class Gcls implements Transformer<Object, Object> {
             String name = TransformerFactory.getUniqueName(transformer);
 
             //noinspection unchecked
-            Optional<Class<?>> defaultParametersConstructor = TransformerFactory
+            Optional<Class<?>> optParametersClass = TransformerFactory
                     .getParameterRichConstructor((Class<? extends Transformer<I, O>>) transformer)
                     .map(constructor -> constructor.getParameterTypes()[0]);
 
-            if (defaultParametersConstructor.isPresent()) {
-                out.accept(context, new Output(name, defaultParametersConstructor.get().getConstructor().newInstance()));
+            if (optParametersClass.isPresent()) {
+                Class<?> parametersClass = optParametersClass.get();
+                out.accept(context, new Output(name, schemaGenerator.generateSchema(parametersClass), parametersClass.getConstructor().newInstance()));
             } else {
                 out.accept(context, new Output(name));
             }
@@ -40,14 +58,16 @@ public final class Gcls implements Transformer<Object, Object> {
     public static class Output {
 
         public String name;
+        public JsonNode schema;
         public Object defaultParameters;
 
         public Output(String name) {
-            this(name, null);
+            this(name, null, null);
         }
 
-        public Output(String name, Object defaultParameters) {
+        public Output(String name, JsonNode schema, Object defaultParameters) {
             this.name = name;
+            this.schema = schema;
             this.defaultParameters = defaultParameters;
         }
     }
