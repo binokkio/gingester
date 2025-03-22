f8504c166330a29008ddd53db494de17945b0a28
Binokkio <binokkio@b.nana.technology>
2025-03-22 17:41:31 +0100

    Set Freemarker output encoding

--- core/src/test/java/b/nana/technology/gingester/core/TemplatingTest.java
+++ core/src/test/java/b/nana/technology/gingester/core/TemplatingTest.java
@@ -13,15 +13,32 @@ public class TemplatingTest {
 
         AtomicReference<String> result = new AtomicReference<>();
 
-        new FlowBuilder().cli("" +
-                "-ss hello world " +
-                "-ss bye world " +
-                "-t FetchMap hello bye " +
-                "-s map " +
-                "-t StringDef '${map}'")
+        new FlowBuilder().cli("""
+                -ss hello world
+                -ss bye world
+                -t FetchMap hello bye
+                -s map
+                -t StringDef '${map}'
+                """)
                 .add(result::set)
                 .run();
 
         assertEquals("{\"hello\":\"world\",\"bye\":\"world\"}", result.get());
+    }
+
+    @Test
+    public void testUrlEncoding() {
+
+        AtomicReference<String> result = new AtomicReference<>();
+
+        new FlowBuilder().cli("""
+                -t StringDef 'Hello, World!'
+                -s helloWorld
+                -t StringDef '${helloWorld?url}'
+                """)
+                .add(result::set)
+                .run();
+
+        assertEquals("Hello%2C%20World!", result.get());
     }
 }
