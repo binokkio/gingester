8a0e98ac33705e0f7bc88d88764225ad66f07724
Binokkio <binokkio@b.nana.technology>
2025-12-09 18:35:30 +0100

    Add Skip transformer

--- core/src/main/java/b/nana/technology/gingester/core/provider/CoreProvider.java
+++ core/src/main/java/b/nana/technology/gingester/core/provider/CoreProvider.java
@@ -42,6 +42,7 @@ public final class CoreProvider implements Provider {
                 b.nana.technology.gingester.core.transformers.primitive.LongToString.class,
                 b.nana.technology.gingester.core.transformers.RatioRoute.class,
                 b.nana.technology.gingester.core.transformers.Repeat.class,
+                b.nana.technology.gingester.core.transformers.Skip.class,
                 b.nana.technology.gingester.core.transformers.stash.FetchAll.class,
                 b.nana.technology.gingester.core.transformers.stash.Fetch.class,
                 b.nana.technology.gingester.core.transformers.stash.FetchMap.class,
